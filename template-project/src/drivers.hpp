3042f0a07044b542dced401d25299c12452b63be
Kaelin Laundry <wasabifan@outlook.com>
2021-07-25 02:51:17 +0000

    rename: misc updates for new name

--- template-project/src/drivers.hpp
+++ template-project/src/drivers.hpp
@@ -1,74 +1,74 @@
 /*
  * Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
  *
- * This file is part of aruwlib.
+ * This file is part of Taproot.
  *
- * aruwlib is free software: you can redistribute it and/or modify
+ * Taproot is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * aruwlib is distributed in the hope that it will be useful,
+ * Taproot is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with aruwlib.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Taproot.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef DRIVERS_HPP_
 #define DRIVERS_HPP_
 
 #if defined(PLATFORM_HOSTED) && defined(ENV_UNIT_TESTS)
-#include "aruwlib/architecture/profiler.hpp"
-#include "aruwlib/mock/analog_mock.hpp"
-#include "aruwlib/mock/can_mock.hpp"
-#include "aruwlib/mock/can_rx_handler_mock.hpp"
-#include "aruwlib/mock/command_mapper_mock.hpp"
-#include "aruwlib/mock/command_scheduler_mock.hpp"
-#include "aruwlib/mock/control_operator_interface_mock.hpp"
-#include "aruwlib/mock/digital_mock.hpp"
-#include "aruwlib/mock/dji_motor_terminal_serial_handler_mock.hpp"
-#include "aruwlib/mock/dji_motor_tx_handler_mock.hpp"
-#include "aruwlib/mock/error_controller_mock.hpp"
-#include "aruwlib/mock/imu_rx_listener_mock.hpp"
-#include "aruwlib/mock/leds_mock.hpp"
-#include "aruwlib/mock/mpu6500_mock.hpp"
-#include "aruwlib/mock/pwm_mock.hpp"
-#include "aruwlib/mock/ref_serial_mock.hpp"
-#include "aruwlib/mock/remote_mock.hpp"
-#include "aruwlib/mock/scheduler_terminal_handler_mock.hpp"
-#include "aruwlib/mock/terminal_serial_mock.hpp"
-#include "aruwlib/mock/uart_mock.hpp"
+#include "tap/architecture/profiler.hpp"
+#include "tap/mock/analog_mock.hpp"
+#include "tap/mock/can_mock.hpp"
+#include "tap/mock/can_rx_handler_mock.hpp"
+#include "tap/mock/command_mapper_mock.hpp"
+#include "tap/mock/command_scheduler_mock.hpp"
+#include "tap/mock/control_operator_interface_mock.hpp"
+#include "tap/mock/digital_mock.hpp"
+#include "tap/mock/dji_motor_terminal_serial_handler_mock.hpp"
+#include "tap/mock/dji_motor_tx_handler_mock.hpp"
+#include "tap/mock/error_controller_mock.hpp"
+#include "tap/mock/imu_rx_listener_mock.hpp"
+#include "tap/mock/leds_mock.hpp"
+#include "tap/mock/mpu6500_mock.hpp"
+#include "tap/mock/pwm_mock.hpp"
+#include "tap/mock/ref_serial_mock.hpp"
+#include "tap/mock/remote_mock.hpp"
+#include "tap/mock/scheduler_terminal_handler_mock.hpp"
+#include "tap/mock/terminal_serial_mock.hpp"
+#include "tap/mock/uart_mock.hpp"
 /* Start user mock includes */
 /* End user mock includes */
 #else
-#include "aruwlib/architecture/profiler.hpp"
-#include "aruwlib/communication/can/can.hpp"
-#include "aruwlib/communication/can/can_rx_handler.hpp"
-#include "aruwlib/communication/can/imu_rx_listener.hpp"
-#include "aruwlib/communication/gpio/analog.hpp"
-#include "aruwlib/communication/gpio/digital.hpp"
-#include "aruwlib/communication/gpio/leds.hpp"
-#include "aruwlib/communication/gpio/pwm.hpp"
-#include "aruwlib/communication/remote.hpp"
-#include "aruwlib/communication/sensors/mpu6500/mpu6500.hpp"
-#include "aruwlib/communication/serial/ref_serial.hpp"
-#include "aruwlib/communication/serial/terminal_serial.hpp"
-#include "aruwlib/communication/serial/uart.hpp"
-#include "aruwlib/control/command_mapper.hpp"
-#include "aruwlib/control/command_scheduler.hpp"
-#include "aruwlib/control/control_operator_interface.hpp"
-#include "aruwlib/control/scheduler_terminal_handler.hpp"
-#include "aruwlib/errors/error_controller.hpp"
-#include "aruwlib/motor/dji_motor_terminal_serial_handler.hpp"
-#include "aruwlib/motor/dji_motor_tx_handler.hpp"
+#include "tap/architecture/profiler.hpp"
+#include "tap/communication/can/can.hpp"
+#include "tap/communication/can/can_rx_handler.hpp"
+#include "tap/communication/can/imu_rx_listener.hpp"
+#include "tap/communication/gpio/analog.hpp"
+#include "tap/communication/gpio/digital.hpp"
+#include "tap/communication/gpio/leds.hpp"
+#include "tap/communication/gpio/pwm.hpp"
+#include "tap/communication/remote.hpp"
+#include "tap/communication/sensors/mpu6500/mpu6500.hpp"
+#include "tap/communication/serial/ref_serial.hpp"
+#include "tap/communication/serial/terminal_serial.hpp"
+#include "tap/communication/serial/uart.hpp"
+#include "tap/control/command_mapper.hpp"
+#include "tap/control/command_scheduler.hpp"
+#include "tap/control/control_operator_interface.hpp"
+#include "tap/control/scheduler_terminal_handler.hpp"
+#include "tap/errors/error_controller.hpp"
+#include "tap/motor/dji_motor_terminal_serial_handler.hpp"
+#include "tap/motor/dji_motor_tx_handler.hpp"
 /* Start user mock includes */
 /* End user mock includes */
 #endif
 
-namespace aruwlib
+namespace tap
 {
 class Drivers
 {
@@ -155,6 +155,6 @@ public:
 #endif
 };  // class Drivers
 
-}  // namespace aruwlib
+}  // namespace tap
 
 #endif  // DRIVERS_HPP_
