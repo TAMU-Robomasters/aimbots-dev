f8272720760fd9254048b7792c69b10017496c9a
Sid Prabhakaran <s2siddhu@gmail.com>
2022-03-28 11:10:16 -0500

    updated  functions and their documentation

--- aimbots-src/src/subsystems/chassis/chassis.cpp
+++ aimbots-src/src/subsystems/chassis/chassis.cpp
@@ -7,91 +7,91 @@
 
 using namespace tap::algorithms;
 
-namespace src::Chassis{
+namespace src::Chassis {
 
-    ChassisSubsystem::ChassisSubsystem(
-        tap::Drivers * drivers) : ChassisSubsystemInterface(drivers),
+ChassisSubsystem::ChassisSubsystem(
+    tap::Drivers* drivers) : ChassisSubsystemInterface(drivers),
 #ifdef TARGET_SENTRY
-    railWheel(drivers, RAIL_WHEEL_ID, CHAS_BUS, false, "Rail Motor"),
+                             railWheel(drivers, RAIL_WHEEL_ID, CHAS_BUS, false, "Rail Motor"),
 #else
-    leftBackWheel(drivers, LEFT_BACK_WHEEL_ID, CHAS_BUS, false, "Left Back Wheel Motor"),
-    leftFrontWheel(drivers, LEFT_FRONT_WHEEL_ID, CHAS_BUS, false, "Left Front Wheel Motor"),
-    rightFrontWheel(drivers, RIGHT_FRONT_WHEEL_ID, CHAS_BUS, false, "Right Front Wheel Motor"),
-    rightBackWheel(drivers, RIGHT_BACK_WHEEL_ID, CHAS_BUS, false, "Right Back Wheel Motor"),
+                             leftBackWheel(drivers, LEFT_BACK_WHEEL_ID, CHAS_BUS, false, "Left Back Wheel Motor"),
+                             leftFrontWheel(drivers, LEFT_FRONT_WHEEL_ID, CHAS_BUS, false, "Left Front Wheel Motor"),
+                             rightFrontWheel(drivers, RIGHT_FRONT_WHEEL_ID, CHAS_BUS, false, "Right Front Wheel Motor"),
+                             rightBackWheel(drivers, RIGHT_BACK_WHEEL_ID, CHAS_BUS, false, "Right Back Wheel Motor"),
 
-    leftBackWheelVelPID(VELOCITY_PID_KP,
-                        VELOCITY_PID_KI,
-                        VELOCITY_PID_KD,
-                        VELOCITY_PID_MAX_ERROR_SUM,
-                        VELOCITY_PID_MAX_OUTPUT),
-    leftFrontWheelVelPID(VELOCITY_PID_KP,
-                         VELOCITY_PID_KI,
-                         VELOCITY_PID_KD,
-                         VELOCITY_PID_MAX_ERROR_SUM,
-                         VELOCITY_PID_MAX_OUTPUT),
-    rightFrontWheelVelPID(VELOCITY_PID_KP,
-                          VELOCITY_PID_KI,
-                          VELOCITY_PID_KD,
-                          VELOCITY_PID_MAX_ERROR_SUM,
-                          VELOCITY_PID_MAX_OUTPUT),
-    rightBackWheelVelPID(VELOCITY_PID_KP,
-                         VELOCITY_PID_KI,
-                         VELOCITY_PID_KD,
-                         VELOCITY_PID_MAX_ERROR_SUM,
-                         VELOCITY_PID_MAX_OUTPUT),
+                             leftBackWheelVelPID(VELOCITY_PID_KP,
+                                                 VELOCITY_PID_KI,
+                                                 VELOCITY_PID_KD,
+                                                 VELOCITY_PID_MAX_ERROR_SUM,
+                                                 VELOCITY_PID_MAX_OUTPUT),
+                             leftFrontWheelVelPID(VELOCITY_PID_KP,
+                                                  VELOCITY_PID_KI,
+                                                  VELOCITY_PID_KD,
+                                                  VELOCITY_PID_MAX_ERROR_SUM,
+                                                  VELOCITY_PID_MAX_OUTPUT),
+                             rightFrontWheelVelPID(VELOCITY_PID_KP,
+                                                   VELOCITY_PID_KI,
+                                                   VELOCITY_PID_KD,
+                                                   VELOCITY_PID_MAX_ERROR_SUM,
+                                                   VELOCITY_PID_MAX_OUTPUT),
+                             rightBackWheelVelPID(VELOCITY_PID_KP,
+                                                  VELOCITY_PID_KI,
+                                                  VELOCITY_PID_KD,
+                                                  VELOCITY_PID_MAX_ERROR_SUM,
+                                                  VELOCITY_PID_MAX_OUTPUT),
 
 #ifdef SWERVE
-    leftBackYaw(drivers, LEFT_BACK_YAW_ID, CHAS_BUS, false, "Left Back Yaw Motor"),
-    leftFrontYaw(drivers, LEFT_FRONT_YAW_ID, CHAS_BUS, false, "Left Front Yaw Motor"),
-    rightFrontYaw(drivers, RIGHT_FRONT_YAW_ID, CHAS_BUS, false, "Right Front Yaw Motor"),
-    rightBackYaw(drivers, RIGHT_BACK_YAW_ID, CHAS_BUS, false, "Right Back Yaw Motor"),
+                             leftBackYaw(drivers, LEFT_BACK_YAW_ID, CHAS_BUS, false, "Left Back Yaw Motor"),
+                             leftFrontYaw(drivers, LEFT_FRONT_YAW_ID, CHAS_BUS, false, "Left Front Yaw Motor"),
+                             rightFrontYaw(drivers, RIGHT_FRONT_YAW_ID, CHAS_BUS, false, "Right Front Yaw Motor"),
+                             rightBackYaw(drivers, RIGHT_BACK_YAW_ID, CHAS_BUS, false, "Right Back Yaw Motor"),
 #endif
 #endif
-    targetRPMs(Matrix<float, DRIVEN_WHEEL_COUNT, MOTORS_PER_WHEEL>::zeroMatrix()),
-    motors(Matrix<DJIMotor*, DRIVEN_WHEEL_COUNT, MOTORS_PER_WHEEL>::zeroMatrix()),
-    velocityPIDs(Matrix<StockPID*, DRIVEN_WHEEL_COUNT, MOTORS_PER_WHEEL>::zeroMatrix()),
-    wheelLocationMatrix(Matrix<float, 4, 3>::zeroMatrix())
-    //
-    {
+                             targetRPMs(Matrix<float, DRIVEN_WHEEL_COUNT, MOTORS_PER_WHEEL>::zeroMatrix()),
+                             motors(Matrix<DJIMotor*, DRIVEN_WHEEL_COUNT, MOTORS_PER_WHEEL>::zeroMatrix()),
+                             velocityPIDs(Matrix<StockPID*, DRIVEN_WHEEL_COUNT, MOTORS_PER_WHEEL>::zeroMatrix()),
+                             wheelLocationMatrix(Matrix<float, 4, 3>::zeroMatrix())
+//
+{
 #ifdef TARGET_SENTRY
-        motors[RAIL][0] = &railWheel;
-velocityPIDs[RAIL][0] = &railWheelVelPID;
+    motors[RAIL][0] = &railWheel;
+    velocityPIDs[RAIL][0] = &railWheelVelPID;
 #else
-        motors[LB][0] = &leftBackWheel;
-motors[LF][0] = &leftFrontWheel;
-motors[RF][0] = &rightFrontWheel;
-motors[RB][0] = &rightBackWheel;
+    motors[LB][0] = &leftBackWheel;
+    motors[LF][0] = &leftFrontWheel;
+    motors[RF][0] = &rightFrontWheel;
+    motors[RB][0] = &rightBackWheel;
 
-velocityPIDs[LB][0] = &leftBackWheelVelPID;
-velocityPIDs[LF][0] = &leftFrontWheelVelPID;
-velocityPIDs[RF][0] = &rightFrontWheelVelPID;
-velocityPIDs[RB][0] = &rightBackWheelVelPID;
+    velocityPIDs[LB][0] = &leftBackWheelVelPID;
+    velocityPIDs[LF][0] = &leftFrontWheelVelPID;
+    velocityPIDs[RF][0] = &rightFrontWheelVelPID;
+    velocityPIDs[RB][0] = &rightBackWheelVelPID;
 
-wheelLocationMatrix[0][0] = -1.0f;
-wheelLocationMatrix[0][1] = 1.0f;
-wheelLocationMatrix[0][2] = WHEELBASE_WIDTH + WHEELBASE_LENGTH;
-wheelLocationMatrix[1][0] = 1.0f;
-wheelLocationMatrix[1][1] = 1.0f;
-wheelLocationMatrix[1][2] = -(WHEELBASE_WIDTH + WHEELBASE_LENGTH);
-wheelLocationMatrix[2][0] = -1.0f;
-wheelLocationMatrix[2][1] = 1.0f;
-wheelLocationMatrix[2][2] = WHEELBASE_WIDTH + WHEELBASE_LENGTH;
-wheelLocationMatrix[3][0] = 1.0f;
-wheelLocationMatrix[3][1] = 1.0f;
-wheelLocationMatrix[3][2] = -(WHEELBASE_WIDTH + WHEELBASE_LENGTH);
+    wheelLocationMatrix[0][0] = -1.0f;
+    wheelLocationMatrix[0][1] = 1.0f;
+    wheelLocationMatrix[0][2] = WHEELBASE_WIDTH + WHEELBASE_LENGTH;
+    wheelLocationMatrix[1][0] = 1.0f;
+    wheelLocationMatrix[1][1] = 1.0f;
+    wheelLocationMatrix[1][2] = -(WHEELBASE_WIDTH + WHEELBASE_LENGTH);
+    wheelLocationMatrix[2][0] = -1.0f;
+    wheelLocationMatrix[2][1] = 1.0f;
+    wheelLocationMatrix[2][2] = WHEELBASE_WIDTH + WHEELBASE_LENGTH;
+    wheelLocationMatrix[3][0] = 1.0f;
+    wheelLocationMatrix[3][1] = 1.0f;
+    wheelLocationMatrix[3][2] = -(WHEELBASE_WIDTH + WHEELBASE_LENGTH);
 
 // NON SWERVE ROBOTS
 #ifdef SWERVE
-// SWERVE ROBOTS
-motors[LB][1] = &leftBackYaw;
-motors[LF][1] = &leftFrontYaw;
-motors[RF][1] = &rightFrontYaw;
-motors[RB][1] = &rightBackYaw;
+    // SWERVE ROBOTS
+    motors[LB][1] = &leftBackYaw;
+    motors[LF][1] = &leftFrontYaw;
+    motors[RF][1] = &rightFrontYaw;
+    motors[RB][1] = &rightBackYaw;
 
-velocityPIDs[LB][1] = &leftBackYawPosPID;
-velocityPIDs[LF][1] = &leftFrontYawPosPID;
-velocityPIDs[RF][1] = &rightFrontYawPosPID;
-velocityPIDs[RB][1] = &rightBackYawPosPID;
+    velocityPIDs[LB][1] = &leftBackYawPosPID;
+    velocityPIDs[LF][1] = &leftFrontYawPosPID;
+    velocityPIDs[RF][1] = &rightFrontYawPosPID;
+    velocityPIDs[RB][1] = &rightBackYawPosPID;
 #endif
 #endif
 }
@@ -106,11 +106,11 @@ void ChassisSubsystem::refresh() {
     ForAllChassisMotors(&ChassisSubsystem::updateMotorVelocityPID);
 }
 
-void ChassisSubsystem::updateMotorVelocityPID(int WheelIdx, int motorPerWheelIdx) {
-    float err = targetRPMs[WheelIdx][motorPerWheelIdx] - motors[WheelIdx][motorPerWheelIdx]->getShaftRPM();
-    velocityPIDs[WheelIdx][motorPerWheelIdx]->update(err);
+void ChassisSubsystem::updateMotorVelocityPID(WheelIndex WheelIdx, MotorOnWheelIndex MotorPerWheelIdx) {
+    float err = targetRPMs[WheelIdx][MotorPerWheelIdx] - motors[WheelIdx][MotorPerWheelIdx]->getShaftRPM();
+    velocityPIDs[WheelIdx][MotorPerWheelIdx]->update(err);
 
-    motors[WheelIdx][motorPerWheelIdx]->setDesiredOutput(velocityPIDs[WheelIdx][motorPerWheelIdx]->getValue());
+    motors[WheelIdx][MotorPerWheelIdx]->setDesiredOutput(velocityPIDs[WheelIdx][MotorPerWheelIdx]->getValue());
 }
 
 void ChassisSubsystem::setDesiredOutputs(float x, float y, float r) {
@@ -182,5 +182,4 @@ float ChassisSubsystem::calculateRotationTranslationalGain(float chassisRotation
 void ChassisSubsystem::calculateSwerve(float, float, float, float) {}
 
 void ChassisSubsystem::calculateRail(float) {}
-}
-;  // namespace src::Chassis
+};  // namespace src::Chassis
--- aimbots-src/src/subsystems/chassis/chassis.hpp
+++ aimbots-src/src/subsystems/chassis/chassis.hpp
@@ -7,59 +7,66 @@
 
 namespace src::Chassis {
 
-    enum WheelIndex {  // index used to easily navigate wheel matrices
-        RAIL = 0,      //	 ___     ___ 	          __
-        LB = 0,        //	| 1 |___| 2 |	    | ___ __|
-        LF = 1,        //	|___| | |___|	    |  | |__
-        RF = 2,        //	      |      	       |
-        RB = 3         //	 ___  |  ___ 	   __  |  __
-                       //	| 0 |_|_| 3 |	  |  |_|_ __|
-                       //	|___|   |___|	  |__|    __|
-    };
+enum WheelIndex {  // index used to easily navigate wheel matrices
+    RAIL = 0,      //	 ___     ___ 	          __
+    LB = 0,        //	| 1 |___| 2 |	    | ___ __|
+    LF = 1,        //	|___| | |___|	    |  | |__
+    RF = 2,        //	      |      	       |
+    RB = 3         //	 ___  |  ___ 	   __  |  __
+                   //	| 0 |_|_| 3 |	  |  |_|_ __|
+                   //	|___|   |___|	  |__|    __|
+};
 
-    class ChassisSubsystem : public tap::control::chassis::ChassisSubsystemInterface {
-       public:
-        ChassisSubsystem(  // Default chassis constructor
-            tap::Drivers* drivers);
+enum MotorOnWheelIndex {
+    DRIVER = 0,  // in this case driver indicates the motor rotating the drive wheels
+    YAW = 1
+};
 
-        /**
-         * Allows user to call a DJIMotor member function on all chassis motors
-         *
-         * @param function pointer to a member function of DJIMotor
-         * @param args arguments to pass to the member function
-         */
-        template <class... Args>
-        void ChassisSubsystem::ForAllChassisMotors(void (DJIMotor::*func)(Args...), Args... args) {
-            for (auto i = 0; i < DRIVEN_WHEEL_COUNT; i++) {
-                (motors[i][0]->*func)(args...);
+class ChassisSubsystem : public tap::control::chassis::ChassisSubsystemInterface {
+   public:
+    ChassisSubsystem(  // Default chassis constructor
+        tap::Drivers* drivers);
+
+    /**
+     * Allows user to call a DJIMotor member function on all chassis motors
+     *
+     * @param function pointer to a member function of DJIMotor
+     * @param args arguments to pass to the member function
+     */
+    template <class... Args>
+    void ForAllChassisMotors(void (DJIMotor::*func)(Args...), Args... args) {
+        for (auto i = 0; i < DRIVEN_WHEEL_COUNT; i++) {
+            (motors[i][0]->*func)(args...);
 #ifdef SWERVE
-                (motors[i][1]->*func)(args...);
+            (motors[i][1]->*func)(args...);
 #endif
-            }
         }
+    }
 
-        /**
-         * Allows user to call a ShooterSubsystem function on all shooter motors.
-         *
-         * @param function pointer to a member function of ShooterSubsystem that takes a MotorIndex as it's first argument
-         * @param args arguments to pass to the member function
-         */
-        template <class... Args>
-        void ChassisSubsystem::ForAllChassisMotors(void (ChassisSubsystem::*func)(int, int, Args...), Args... args) {
-            for (auto i = 0; i < DRIVEN_WHEEL_COUNT; i++) {
-                (this->*func)(i, 0, args...);
+    /**
+     * Allows user to call a ChassisSubsystem function on all chassis motors.
+     *
+     * @param function pointer to a member function of ChassisSubsystem that takes a WheelIndex
+     * as its first argument and the motor-per-wheel index as the second argument
+     * @param args arguments to pass to the member function
+     */
+    template <class... Args>
+    void ForAllChassisMotors(void (ChassisSubsystem::*func)(WheelIndex, MotorOnWheelIndex, Args...), Args... args) {
+        for (auto i = 0; i < DRIVEN_WHEEL_COUNT; i++) {
+            WheelIndex mi = static_cast<WheelIndex>(i);
+            (this->*func)(mi, DRIVER, args...);
 #ifdef SWERVE
-                (this->*func)(i, 1, args...);
+            (this->*func)(mi, YAW, args...);
 #endif
-            }
         }
+    }
 
-        mockable void initialize() override;
-        void refresh() override;
+    mockable void initialize() override;
+    void refresh() override;
 
-        void updateMotorVelocityPID(int WheelIdx, int motorPerWheelIdx);
+    void updateMotorVelocityPID(WheelIndex WheelIdx, MotorOnWheelIndex MotorOnWheelIdx);
 
-        /**
+    /**
      * @brief Updates the desired wheel RPM based on the input x, y, and rotation movement components.
      *
      * @param[in] x The desired velocity of the wheels to move in the X (horizontal) direction.
@@ -69,91 +76,91 @@ namespace src::Chassis {
      * @param[in] r The desired velocity of the wheels to rotate.
      * See x for more information.
      */
-        void setDesiredOutputs(float x, float y, float r);
+    void setDesiredOutputs(float x, float y, float r);
 
-        void calculateMecanum(float x, float y, float r, float maxWheelSpeed);  // normal 4wd mecanum robots
-        void calculateSwerve(float x, float y, float r, float maxWheelSpeed);   // swerve drive robots
-        void calculateRail(float x);                                            // sentry rail robots
+    void calculateMecanum(float x, float y, float r, float maxWheelSpeed);  // normal 4wd mecanum robots
+    void calculateSwerve(float x, float y, float r, float maxWheelSpeed);   // swerve drive robots
+    void calculateRail(float x);                                            // sentry rail robots
 
-        inline int getNumChassisMotors() const override { return DRIVEN_WHEEL_COUNT * MOTORS_PER_WHEEL; }
+    inline int getNumChassisMotors() const override { return DRIVEN_WHEEL_COUNT * MOTORS_PER_WHEEL; }
 
-        /**
+    /**
      * @return A number between 0 and 1 that is the ratio between the rotationRpm and
      *      the max rotation speed.
      */
-        mockable float calculateRotationTranslationalGain(float chassisRotationDesiredWheelspeed);
+    mockable float calculateRotationTranslationalGain(float chassisRotationDesiredWheelspeed);
 
-        static inline float getMaxUserWheelSpeed(bool refSerialOnline, int chassisPower) {
-            if (refSerialOnline) {
-                float desWheelSpeed = WHEEL_SPEED_OVER_CHASSIS_POWER_SLOPE *
-                                          static_cast<float>(chassisPower - MIN_CHASSIS_POWER) +
-                                      MIN_WHEEL_SPEED_SINGLE_MOTOR;
+    static inline float getMaxUserWheelSpeed(bool refSerialOnline, int chassisPower) {
+        if (refSerialOnline) {
+            float desWheelSpeed = WHEEL_SPEED_OVER_CHASSIS_POWER_SLOPE *
+                                      static_cast<float>(chassisPower - MIN_CHASSIS_POWER) +
+                                  MIN_WHEEL_SPEED_SINGLE_MOTOR;
 
-                return tap::algorithms::limitVal(
-                    desWheelSpeed,
-                    static_cast<float>(MIN_WHEEL_SPEED_SINGLE_MOTOR),
-                    static_cast<float>(MAX_WHEEL_SPEED_SINGLE_MOTOR));
-            } else {
-                return MIN_WHEEL_SPEED_SINGLE_MOTOR;
-            }
+            return tap::algorithms::limitVal(
+                desWheelSpeed,
+                static_cast<float>(MIN_WHEEL_SPEED_SINGLE_MOTOR),
+                static_cast<float>(MAX_WHEEL_SPEED_SINGLE_MOTOR));
+        } else {
+            return MIN_WHEEL_SPEED_SINGLE_MOTOR;
         }
+    }
 
-        inline bool allMotorsOnline() const override {
-            for (int i = 0; i < DRIVEN_WHEEL_COUNT; i++) {
-                for (int j = 0; j < MOTORS_PER_WHEEL; j++) {
-                    if (!motors[i][j]->isMotorOnline()) {
-                        return false;
-                    }
+    inline bool allMotorsOnline() const override {
+        for (int i = 0; i < DRIVEN_WHEEL_COUNT; i++) {
+            for (int j = 0; j < MOTORS_PER_WHEEL; j++) {
+                if (!motors[i][j]->isMotorOnline()) {
+                    return false;
                 }
             }
-            return true;
         }
+        return true;
+    }
 
 #ifndef ENV_UNIT_TESTS
-       private:
+   private:
 #else
-       public:
+   public:
 #endif
-        static constexpr CANBus CHAS_BUS = CANBus::CAN_BUS2;
+    static constexpr CANBus CHAS_BUS = CANBus::CAN_BUS2;
 
 #ifdef TARGET_SENTRY
-        DJIMotor railWheel;
-        StockPID railWheelVelPID;
+    DJIMotor railWheel;
+    StockPID railWheelVelPID;
 #else
-        DJIMotor leftBackWheel, leftFrontWheel, rightFrontWheel, rightBackWheel;
-        StockPID leftBackWheelVelPID, leftFrontWheelVelPID, rightFrontWheelVelPID, rightBackWheelVelPID;
-        float desiredRotation = 0.0f;
+    DJIMotor leftBackWheel, leftFrontWheel, rightFrontWheel, rightBackWheel;
+    StockPID leftBackWheelVelPID, leftFrontWheelVelPID, rightFrontWheelVelPID, rightBackWheelVelPID;
+    float desiredRotation = 0.0f;
 #ifdef SWERVE
-        DJIMotor leftBackYaw, leftFrontYaw, rightFrontYaw, rightBackYaw;
-        StockPID leftBackYawPosPID, leftFrontYawPosPID, rightFrontYawPosPID, rightBackYawPosPID;
+    DJIMotor leftBackYaw, leftFrontYaw, rightFrontYaw, rightBackYaw;
+    StockPID leftBackYawPosPID, leftFrontYawPosPID, rightFrontYawPosPID, rightBackYawPosPID;
 #endif
 #endif
 
-        Matrix<float, DRIVEN_WHEEL_COUNT, MOTORS_PER_WHEEL> targetRPMs;
+    Matrix<float, DRIVEN_WHEEL_COUNT, MOTORS_PER_WHEEL> targetRPMs;
 
-        Matrix<DJIMotor*, DRIVEN_WHEEL_COUNT, MOTORS_PER_WHEEL> motors;
+    Matrix<DJIMotor*, DRIVEN_WHEEL_COUNT, MOTORS_PER_WHEEL> motors;
 
-        // ChassisPowerLimiter powerLimiter;
+    // ChassisPowerLimiter powerLimiter;
 
-        Matrix<StockPID*, DRIVEN_WHEEL_COUNT, MOTORS_PER_WHEEL> velocityPIDs;
+    Matrix<StockPID*, DRIVEN_WHEEL_COUNT, MOTORS_PER_WHEEL> velocityPIDs;
 
-        Matrix<float, 4, 3> wheelLocationMatrix;
+    Matrix<float, 4, 3> wheelLocationMatrix;
 
-       public:
+   public:
 #ifdef TARGET_SENTRY
-        inline int16_t
-        getLeftFrontRpmActual() const override {
-            return railWheel.getShaftRPM();
-        }
-        inline int16_t getLeftBackRpmActual() const override { return railWheel.getShaftRPM(); }
-        inline int16_t getRightFrontRpmActual() const override { return railWheel.getShaftRPM(); }
-        inline int16_t getRightBackRpmActual() const override { return railWheel.getShaftRPM(); }
+    inline int16_t
+    getLeftFrontRpmActual() const override {
+        return railWheel.getShaftRPM();
+    }
+    inline int16_t getLeftBackRpmActual() const override { return railWheel.getShaftRPM(); }
+    inline int16_t getRightFrontRpmActual() const override { return railWheel.getShaftRPM(); }
+    inline int16_t getRightBackRpmActual() const override { return railWheel.getShaftRPM(); }
 #else
-        inline int16_t getLeftFrontRpmActual() const override { return leftFrontWheel.getShaftRPM(); }
-        inline int16_t getLeftBackRpmActual() const override { return leftBackWheel.getShaftRPM(); }
-        inline int16_t getRightFrontRpmActual() const override { return rightFrontWheel.getShaftRPM(); }
-        inline int16_t getRightBackRpmActual() const override { return rightBackWheel.getShaftRPM(); }
+    inline int16_t getLeftFrontRpmActual() const override { return leftFrontWheel.getShaftRPM(); }
+    inline int16_t getLeftBackRpmActual() const override { return leftBackWheel.getShaftRPM(); }
+    inline int16_t getRightFrontRpmActual() const override { return rightFrontWheel.getShaftRPM(); }
+    inline int16_t getRightBackRpmActual() const override { return rightBackWheel.getShaftRPM(); }
 #endif
-    };
+};
 
 };  // namespace src::Chassis
