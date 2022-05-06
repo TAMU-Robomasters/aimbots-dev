4bd5e3b2ce7d998c6d25cba63c842460b4596f3c
ErrorOnUsername <richarddavidson000@gmail.com>
2022-05-05 20:34:53 -0500

    magnetometer: potential comms fix for ist8310

--- aimbots-src/src/utils/custom_imu/magnetometer/ist8310.hpp
+++ aimbots-src/src/utils/custom_imu/magnetometer/ist8310.hpp
@@ -21,21 +21,26 @@ class Ist8310 : modm::I2cDevice<Ist8310Data::IST_I2C_MASTER> {
    private:
     inline modm::ResumableResult<bool> readRegister(Ist8310Data::Register reg, size_t length = 1) {
         RF_BEGIN();
-        (void)length;
 
         raw_data_buffer[0] = uint8_t(reg);
-        this->transaction.configureWrite(raw_data_buffer, 1);
+        transaction.configureWrite(raw_data_buffer, 1);
+        runTransaction();
 
-        RF_END_RETURN_CALL(this->runTransaction());
+        transaction.configureRead(raw_data_buffer, length);
+
+        RF_END_RETURN_CALL(runTransaction());
     }
 
     inline modm::ResumableResult<bool> writeToRegister(Ist8310Data::Register reg, uint8_t* output, size_t length = 1) {
         RF_BEGIN();
 
         raw_data_buffer[0] = uint8_t(reg);
-        this->transaction.configureWriteRead(raw_data_buffer, 1, output, length);
+        transaction.configureWrite(raw_data_buffer, 1);
+        runTransaction();
 
-        RF_END_RETURN_CALL(this->runTransaction());
+        transaction.configureWrite(output, length);
+
+        RF_END_RETURN_CALL(runTransaction());
     }
 
     inline uint8_t readDeviceID() {
@@ -48,7 +53,7 @@ class Ist8310 : modm::I2cDevice<Ist8310Data::IST_I2C_MASTER> {
         // Don't continue if the data-ready bit isn't set
         if(!(raw_data_buffer[0] & uint8_t(Ist8310Data::RegisterData::STAT1_DATA_READY_MASK))) { return false; }
 
-        readRegister(Ist8310Data::Register::X_DATA_LOW, 6);
+        readRegister(Ist8310Data::Register::X_DATA_LOW, sizeof(uint16_t) * 3);
         return true;
     }
 
