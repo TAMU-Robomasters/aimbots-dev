4864de4af0d67b8b68d053e64260a9d88c2fd0d8
Sid Prabhakaran <s2siddhu@gmail.com>
2022-05-13 15:46:49 -0500

    started over with updateSerial

--- aimbots-src/src/utils/jetson_communicator.cpp
+++ aimbots-src/src/utils/jetson_communicator.cpp
@@ -27,55 +27,10 @@ float pitchOffsetDisplay = 0;
 CVState cvStateDisplay = CVState::CV_STATE_PATROL;
 size_t rxBufferSize = 0;
 
+/**
+ * @brief Need to use modm's uart functions to read from the Jetson.
+ * The Jetson sends information in the form of a JetsonMessage.
+ */
 void JetsonCommunicator::updateSerial() {
-    switch (currentSerialState) {
-        case JetsonCommunicatorSerialState::SearchingForMagic: {
-            size_t bytesRead = READ(&rawSerialBuffer[nextByteIndex], 1);
-            if (bytesRead != 1) return;
-
-            // We've gotten data from the Jetson, so we can restart this.
-            jetsonOfflineTimeout.restart(JETSON_OFFLINE_TIMEOUT_MILLISECONDS);
-
-            // little endian moment
-            // The the first byte in the message will be the LSB of the magic number,
-            // so we only have to and it with 0xff, rather than shifting it right first.
-            if (rawSerialBuffer[nextByteIndex] == ((JETSON_MESSAGE_MAGIC >> (8 * nextByteIndex)) & 0xff))
-                nextByteIndex++;
-
-            if (nextByteIndex == sizeof(decltype(JETSON_MESSAGE_MAGIC))) {
-                // We know that the magic is right, so we can just change the state. If the
-                // magic number wasn't an exact match, we wouldn't have gotten this far.
-                currentSerialState = JetsonCommunicatorSerialState::AssemblingMessage;
-            }
-            break;
-        }
-        case JetsonCommunicatorSerialState::AssemblingMessage: {
-            size_t bytesRead = READ(&rawSerialBuffer[nextByteIndex], 1);
-            if (bytesRead != 1) {
-                return;
-            } else {
-                nextByteIndex++;
-            }
-
-            // We've gotten data from the Jetson, so we can restart this.
-            jetsonOfflineTimeout.restart(JETSON_OFFLINE_TIMEOUT_MILLISECONDS);
-
-            if (nextByteIndex == sizeof(JetsonMessage)) {
-                lastMessage = *reinterpret_cast<JetsonMessage*>(&rawSerialBuffer);
-
-                memcpy(displayBuffer, rawSerialBuffer, sizeof(displayBuffer));
-
-                yawOffsetDisplay = lastMessage.targetYawOffset;
-                pitchOffsetDisplay = lastMessage.targetPitchOffset;
-                cvStateDisplay = lastMessage.cvState;
-
-                // uint8_t* buffer = reinterpret_cast<uint8_t*>(&lastMessage);
-                // WRITE(buffer, sizeof(decltype(JETSON_MESSAGE_MAGIC)));
-                currentSerialState = JetsonCommunicatorSerialState::SearchingForMagic;
-                nextByteIndex = 0;
-            }
-            break;
-        }
-    }
 }
 }
