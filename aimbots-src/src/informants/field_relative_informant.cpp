c224bf57d8d487d5b965ff1496c3e849d5e2da64
Sid Prabhakaran <s2siddhu@gmail.com>
2022-06-01 15:18:31 -0500

    added fix for patrol index bouncing

--- aimbots-src/src/informants/field_relative_informant.cpp
+++ aimbots-src/src/informants/field_relative_informant.cpp
@@ -5,44 +5,44 @@
 
 namespace src::Informants {
 
-    FieldRelativeInformant::FieldRelativeInformant(src::Drivers * drivers)
-        : drivers(drivers),
-          robotStartingPosition(ROBOT_STARTING_POSITION) {}
+FieldRelativeInformant::FieldRelativeInformant(src::Drivers* drivers)
+    : drivers(drivers),
+      robotStartingPosition(ROBOT_STARTING_POSITION) {}
 
-    void FieldRelativeInformant::initialize() {}
+void FieldRelativeInformant::initialize() {}
 
-    float robotPositionXDisplay = 0.0f;
-    float robotPositionYDisplay = 0.0f;
-    float robotPositionZDisplay = 0.0f;
+float robotPositionXDisplay = 0.0f;
+float robotPositionYDisplay = 0.0f;
+float robotPositionZDisplay = 0.0f;
 
-    void FieldRelativeInformant::updateFieldRelativeRobotPosition(DJIMotor * cMotor) {
+void FieldRelativeInformant::updateFieldRelativeRobotPosition(DJIMotor* cMotor) {
 #ifdef TARGET_SENTRY  // This will need to be replaced with code that uses the ultrasonics once that works
-        // first, get unwrapped motor position
-        float motorRevolutionsUnwrapped = static_cast<float>(cMotor->getEncoderUnwrapped()) / static_cast<float>(cMotor->ENC_RESOLUTION);  // current position in motor revolutions
-        // now, convert to unwrapped wheel revolutions
-        float wheelRevolutionsUnwrapped = motorRevolutionsUnwrapped * CHASSIS_GEARBOX_RATIO;  // current position in wheel revolutions
-        // now, convert to unwrapped wheel rotations to get the rail position
-        float currRailPosition = -wheelRevolutionsUnwrapped * (2 * M_PI * WHEEL_RADIUS);  // current position in meters
+    // first, get unwrapped motor position
+    float motorRevolutionsUnwrapped = static_cast<float>(cMotor->getEncoderUnwrapped()) / static_cast<float>(cMotor->ENC_RESOLUTION);  // current position in motor revolutions
+    // now, convert to unwrapped wheel revolutions
+    float wheelRevolutionsUnwrapped = motorRevolutionsUnwrapped * CHASSIS_GEARBOX_RATIO;  // current position in wheel revolutions
+    // now, convert to unwrapped wheel rotations to get the rail position
+    float currRailPosition = -wheelRevolutionsUnwrapped * (2 * M_PI * WHEEL_RADIUS);  // current position in meters
 
-        // set the current rail position to a position matrix relative to the rail
-        Matrix<float, 1, 3> railRelativePosition = Matrix<float, 1, 3>::zeroMatrix();
-        railRelativePosition[0][0] = currRailPosition;
+    // set the current rail position to a position matrix relative to the rail
+    Matrix<float, 1, 3> railRelativePosition = Matrix<float, 1, 3>::zeroMatrix();
+    railRelativePosition[0][0] = currRailPosition;
 
-        // rotate the matrix by 45 degrees (rail is mounted at 45 degree angle) and add to the robot's starting position
-        fieldRelativeRobotPosition = railRelativePosition * xy_rotation_matrix(AngleUnit::Degrees, 45.0f) + ROBOT_STARTING_POSITION;
+    // rotate the matrix by 45 degrees (rail is mounted at 45 degree angle) and add to the robot's starting position
+    fieldRelativeRobotPosition = railRelativePosition * src::utils::MatrixHelper::xy_rotation_matrix(AngleUnit::Degrees, 45.0f) + ROBOT_STARTING_POSITION;
 
-        robotPositionXDisplay = fieldRelativeRobotPosition[0][0];
-        robotPositionYDisplay = fieldRelativeRobotPosition[0][1];
-        robotPositionZDisplay = fieldRelativeRobotPosition[0][2];
+    robotPositionXDisplay = fieldRelativeRobotPosition[0][0];
+    robotPositionYDisplay = fieldRelativeRobotPosition[0][1];
+    robotPositionZDisplay = fieldRelativeRobotPosition[0][2];
 #endif
-    }
-
-    // gets the angle between the robot's current position and the field coordinate
-    float FieldRelativeInformant::getXYAngleToFieldCoordinate(AngleUnit unit, Matrix<float, 1, 3> fieldCoordinate) {
-        float xy_angle = xy_angle_between_locations(AngleUnit::Radians, fieldRelativeRobotPosition, fieldCoordinate);
-        if (unit == AngleUnit::Degrees) {
-            xy_angle = modm::toDegree(xy_angle);
-        }
-        return xy_angle;
-    }
 }
+
+// gets the angle between the robot's current position and the field coordinate
+float FieldRelativeInformant::getXYAngleToFieldCoordinate(AngleUnit unit, Matrix<float, 1, 3> fieldCoordinate) {
+    float xy_angle = src::utils::MatrixHelper::xy_angle_between_locations(AngleUnit::Radians, fieldRelativeRobotPosition, fieldCoordinate);
+    if (unit == AngleUnit::Degrees) {
+        xy_angle = modm::toDegree(xy_angle);
+    }
+    return xy_angle;
+}
+}  // namespace src::Informants
--- aimbots-src/src/utils/math/matrix_helpers.cpp
+++ aimbots-src/src/utils/math/matrix_helpers.cpp
@@ -1,21 +1,10 @@
 #include "utils/common_types.hpp"
 
-float v1XDisplay = 0.0f;
-float v1YDisplay = 0.0f;
-float v2XDisplay = 0.0f;
-float v2YDisplay = 0.0f;
-float v3XDisplay = 0.0f;
-float v3YDisplay = 0.0f;
+namespace src::utils::MatrixHelper {
 
 float xy_angle_between_locations(AngleUnit unit, Matrix<float, 1, 3> v1, Matrix<float, 1, 3> v2) {
     Matrix<float, 1, 3> v3 = v2 - v1;
 
-    v1XDisplay = v1[0][0];
-    v1YDisplay = v1[0][1];
-    v2XDisplay = v2[0][0];
-    v2YDisplay = v2[0][1];
-    v3XDisplay = v3[0][0];
-    v3YDisplay = v3[0][1];
     float angle = atan2f(v3[0][1], v3[0][0]);
     if (unit == AngleUnit::Degrees) {
         angle = modm::toDegree(angle);
@@ -37,3 +26,5 @@ Matrix<float, 3, 3> xy_rotation_matrix(AngleUnit unit, float angle) {
     // clang-format on
     return Matrix<float, 3, 3>(xy_rotation_array);
 }
+
+}  // namespace src::utils::MatrixHelper
