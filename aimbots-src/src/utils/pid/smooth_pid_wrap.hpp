34ffece24faa8146b86f153327f714e35a95183a
Sid Prabhakaran <s2siddhu@gmail.com>
2022-06-02 14:39:28 -0500

    moved settled_util to own class

--- aimbots-src/src/utils/pid/smooth_pid_wrap.hpp
+++ aimbots-src/src/utils/pid/smooth_pid_wrap.hpp
@@ -1,75 +1,60 @@
 #pragma once
 
 #include <tap/algorithms/smooth_pid.hpp>
+#include "utils/motion/settled_util.hpp"
 
 namespace src::utils {
 
-struct SmoothPIDWrapper {
-    float lastTime;
-    float error;
-    float prevError;
-    float errorDerivative;
+    struct SmoothPIDWrapper {
+        float lastTime;
+        float error;
+        float prevError;
+        float errorDerivative;
 
-    tap::arch::MilliTimeout errorTimeout;
-    tap::arch::MilliTimeout derivativeTimeout;
+        SettledUtil settledUtil;
+        tap::algorithms::SmoothPid pid;
 
-    tap::algorithms::SmoothPid pid;
+        SmoothPIDWrapper(const tap::algorithms::SmoothPidConfig &config) : pid(config) {}
 
-    SmoothPIDWrapper(const tap::algorithms::SmoothPidConfig &config) : pid(config) {}
-
-    float runController(float error, float derivativeInput) {
-        this->error = error;
-        this->errorDerivative = derivativeInput;
-        float currTime = static_cast<float>(tap::arch::clock::getTimeMilliseconds());
-        float dt = currTime - lastTime;
-        lastTime = currTime;
-        return pid.runController(error, derivativeInput, dt);
-    }
-
-    float runControllerDerivateError(float error) {
-        this->error = error;
-        float currTime = static_cast<float>(tap::arch::clock::getTimeMilliseconds());
-        float dt = currTime - lastTime;
-        lastTime = currTime;
-
-        errorDerivative = (error - prevError) / dt;
-
-        return pid.runControllerDerivateError(error, dt);
-    }
-
-    float isSettled(float errTolerance) {
-        return static_cast<float>(fabs(error)) < errTolerance;
-    }
-
-    float isSettled(float errTolerance, float derivTolerance, float derivToleranceTime) {
-        if (static_cast<float>(fabs(error)) < errTolerance) {
-            if (static_cast<float>(fabs(errorDerivative)) < derivTolerance) {
-                // if timeout has expired and it's still running, then it's settled
-                if (derivativeTimeout.isExpired()) {
-                    return true;
-                } else if (derivativeTimeout.isStopped()) {
-                    // otherwise if the timeout has been stopped, then it must've previously left the settled range and needs restarting
-                    derivativeTimeout.restart(derivToleranceTime);
-                }
-            } else {
-                // if the derivative is out of the tolerance range, then stop the timeout
-                derivativeTimeout.stop();
-            }
+        float runController(float error, float derivativeInput) {
+            this->error = error;
+            this->errorDerivative = derivativeInput;
+            float currTime = static_cast<float>(tap::arch::clock::getTimeMilliseconds());
+            float dt = currTime - lastTime;
+            lastTime = currTime;
+            return pid.runController(error, derivativeInput, dt);
         }
-        return false;
-    }
 
-    float getError() {
-        return this->error;
-    }
+        float runControllerDerivateError(float error) {
+            this->error = error;
+            float currTime = static_cast<float>(tap::arch::clock::getTimeMilliseconds());
+            float dt = currTime - lastTime;
+            lastTime = currTime;
 
-    float getDerivative() {
-        return this->errorDerivative;
-    }
+            errorDerivative = (error - prevError) / dt;
 
-    float getOutput() {
-        return pid.getOutput();
-    }
-};
+            return pid.runControllerDerivateError(error, dt);
+        }
+
+        bool isSettled(float errTolerance) {
+            return settledUtil.isSettled(this->error, errTolerance);
+        }
+
+        bool isSettled(float errTolerance, float derivTolerance, float derivToleranceTime) {
+            return settledUtil.isSettled(this->error, errTolerance, this->errorDerivative, derivTolerance, derivToleranceTime);
+        }
+
+        float getError() {
+            return this->error;
+        }
+
+        float getDerivative() {
+            return this->errorDerivative;
+        }
+
+        float getOutput() {
+            return pid.getOutput();
+        }
+    };
 
 }  // namespace src::utils
