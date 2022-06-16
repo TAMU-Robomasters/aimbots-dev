61f365591f3da79e96957f748999a50012048394
Sid Prabhakaran <s2siddhu@gmail.com>
2022-06-16 10:15:27 -0700

    updated Standard's dev board offset

--- aimbots-src/src/robots/standard/standard_constants.hpp
+++ aimbots-src/src/robots/standard/standard_constants.hpp
@@ -11,7 +11,7 @@ static constexpr uint8_t MOTORS_PER_WHEEL = 1;
 static constexpr uint8_t SHOOTER_MOTOR_COUNT = 2;
 
 // in radians
-static constexpr float DEV_BOARD_YAW_OFFSET = -M_PI_2;
+static constexpr float DEV_BOARD_YAW_OFFSET = 0.0f;
 
 /**
  * @brief Definitions for operator interface constants (may change based on preference of drivers)
