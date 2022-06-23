5c0c5f1e9b442303e6fd54e337ce9c4051669495
Jeff Hykin <jeff.hykin@gmail.com>
2022-06-23 12:41:13 -0500

    add new cv protocol

--- aimbots-src/src/informants/vision/jetson_protocol.hpp
+++ aimbots-src/src/informants/vision/jetson_protocol.hpp
@@ -3,27 +3,20 @@
 namespace src::Informants::vision {
 
 enum CVState : uint8_t {
-    CV_STATE_UNSURE = 0,
-    CV_STATE_FOUND = 1,
+    TRACK = 0,
+    SHOOT = 1,
+    PATROL = 2,
 };
 
-// static constexpr uint64_t JETSON_MESSAGE_MAGIC = 0xdeadbeefdeadbef;
 static constexpr uint8_t JETSON_MESSAGE_MAGIC = 'a';
 
-// struct JetsonMessage {
-//     uint64_t magic;
-//     float targetYawOffset;
-//     float targetPitchOffset;
-//     CVState cvState;
-// } __attribute__((packed));
 struct JetsonMessage {
-    uint8_t magic;
-    float targetYawOffset;
-    float targetPitchOffset;
-    CVState cvState;
+    uint8_t magic;           // 97
+    float targetYawOffset;   // units=radians if target is to the left of camera-center, this will be negative
+    float targetPitchOffset; // units=radians if target is below camera-center, this will be positive for some reason
+    CVState cvState;         // 0 is track, 1 is shoot, 2 is patrol
 } __attribute__((packed));
 
-// static_assert(sizeof(JetsonMessage) == 17, "JetsonMessage is not the correct size");
 static_assert(sizeof(JetsonMessage) == 10, "JetsonMessage is not the correct size");
 
 static constexpr size_t JETSON_MESSAGE_SIZE = sizeof(JetsonMessage);
