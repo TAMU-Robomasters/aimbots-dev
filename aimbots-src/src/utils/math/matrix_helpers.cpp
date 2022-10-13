c1e0a2ef60e03822d833c33286ab6ce694be95f4
LukeBatteas <luke.batteas@tamu.edu>
2022-10-12 20:18:50 -0700

    Updated Comments in Matrix Helper - Untested

--- aimbots-src/src/utils/math/matrix_helpers.cpp
+++ aimbots-src/src/utils/math/matrix_helpers.cpp
@@ -4,6 +4,7 @@ namespace src::utils::MatrixHelper {
 
 float xy_angle_between_locations(AngleUnit unit, Matrix<float, 1, 3> v1, Matrix<float, 1, 3> v2) {
     // Gets the absolute angle from the horizontal in the positive direction of the vector from one point to another
+    // Used to determine the absolute, field relative angle to aim at from our robot positon (v1) to another postion (v2)
     // Define the vector from point 2 to point 1
     Matrix<float, 1, 3> v3 = v2 - v1;
 
@@ -34,6 +35,7 @@ Matrix<float, 3, 3> xy_rotation_matrix(AngleUnit unit, float angle) {
 
 Matrix<float, 3, 3> rotation_matrix(AngleUnit unit, float angle, int axis) {
     // Constructs a rotation matrix about an elementary (Robot's X, Y, Z Axis)
+    // Rotation matricies are used to determine coordinates before / after a rotation
     // Takes input of angle unit (Degrees / Rads), Float angle, and axis
     //  0 = X - Axis
     //  1 = Y - Axis
@@ -59,7 +61,7 @@ Matrix<float, 3, 3> rotation_matrix(AngleUnit unit, float angle, int axis) {
             // clang-format off
              rotation_array[0] = 1.0f; rotation_array[1] = 0.0f; rotation_array[2] = 0.0f;
              rotation_array[3] = 0.0f; rotation_array[4] = c; rotation_array[5] = -s;
-             rotation_array[6] = 0.0f; rotation_array[7] = s; rotation_array[9] = c;
+             rotation_array[6] = 0.0f; rotation_array[7] = s; rotation_array[8] = c;
             // clang-format on
             break;
         }
@@ -70,7 +72,7 @@ Matrix<float, 3, 3> rotation_matrix(AngleUnit unit, float angle, int axis) {
             // clang-format off
             rotation_array[0] = c; rotation_array[1] = 0.0f; rotation_array[2] = s;
             rotation_array[3] = 0.0f; rotation_array[4] = 1.0; rotation_array[5] = 0.0f;
-            rotation_array[6] = -s; rotation_array[7] = 0.0f; rotation_array[9] = c;
+            rotation_array[6] = -s; rotation_array[7] = 0.0f; rotation_array[8] = c;
             // clang-format on
             break;
         }
@@ -80,7 +82,7 @@ Matrix<float, 3, 3> rotation_matrix(AngleUnit unit, float angle, int axis) {
             // clang-format off
             rotation_array[0] = c; rotation_array[1] = -s; rotation_array[2] = 0.0f;
             rotation_array[3] = s; rotation_array[4] = c; rotation_array[5] = 0.0f;
-            rotation_array[6] = 0.0f; rotation_array[7] = 0.0f; rotation_array[9] = 1.0f;
+            rotation_array[6] = 0.0f; rotation_array[7] = 0.0f; rotation_array[8] = 1.0f;
             // clang-format on
             break;
         }
---
+++ aimbots-src/test/utils/Math/matrix_test.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ *
+ * This file is part of aimbots-src.
+ *
+ * aimbots-src is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aimbots-src is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aimbots-src.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include <gtest/gtest.h>
+
+#include "utils/common_types.hpp"
+#include "utils/math/matrix_helpers.hpp"
+
+using ::testing::NiceMock;
+
+TEST(MatrixConstructorTest, rotation_matrix_creates_rot_matricies) {
+    // ON_CALL(c.leftBackWheel, getEncoderUnwrapped);
+
+    // clang-format off
+    //define identity matrix
+    float rotation_array[9] = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+    //check if any identity returns
+    EXPECT_EQ(Matrix<float, 3, 3>(rotation_array), rotation_matrix(AngleUnit::Degrees, 45, 3));
+            //define Rx
+            rotation_array[0] = 1.0f; rotation_array[1] = 0.0f; rotation_array[2] = 0.0f;
+            rotation_array[3] = 0.0f; rotation_array[4] = c; rotation_array[5] = -s;
+            rotation_array[6] = 0.0f; rotation_array[7] = s; rotation_array[8] = c;
+     EXPECT_EQ(Matrix<float, 3, 3>(rotation_array),rotation_matrix(AngleUnit::Degrees, 45, 0));
+            //define Ry
+            rotation_array[0] = c; rotation_array[1] = 0.0f; rotation_array[2] = s;
+            rotation_array[3] = 0.0f; rotation_array[4] = 1.0; rotation_array[5] = 0.0f;
+            rotation_array[6] = -s; rotation_array[7] = 0.0f; rotation_array[8] = c;
+     EXPECT_EQ(Matrix<float, 3, 3>(rotation_array),rotation_matrix(AngleUnit::Degrees, 45, 1));
+            //define Rz
+            rotation_array[0] = c; rotation_array[1] = -s; rotation_array[2] = 0.0f;
+            rotation_array[3] = s; rotation_array[4] = c; rotation_array[5] = 0.0f;
+            rotation_array[6] = 0.0f; rotation_array[7] = 0.0f; rotation_array[8] = 1.0f;
+     EXPECT_EQ(Matrix<float, 3, 3>(rotation_array),rotation_matrix(AngleUnit::Degrees, 45, 2));
+    // clang-format on
+}
+
+// TEST(hello, world) { EXPECT_EQ(3, sum(1, 2)); }
