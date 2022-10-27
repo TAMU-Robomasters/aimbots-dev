276733e1503357c0256a84febb84e3960719725b
sebvc <90735870+sebvc@users.noreply.github.com>
2022-10-26 20:12:58 -0500

    transformations set up and R&P matricies

--- aimbots-src/src/utils/math/transform_setup.hpp
+++ aimbots-src/src/utils/math/transform_setup.hpp
@@ -5,8 +5,9 @@
 namespace src::utils::MatrixHelper {
 
 // returns combined Rotation Matrix R13 = [R12]*[R23]
-Matrix<float, 3, 3> merge_rotation_matrix(Matrix<float, 3, 3> R12, Matrix<float, 3, 3> R23) {
+Matrix<float, 3, 3> merge_rotation_matrix(Matrix<float, 3, 3> R12, Matrix<float, 3, 3> R23);
 
-Matrix<float, 4, 4> transform_matrix(Matrix<float, 3, 3> R_CG, Matrix<float, 3, 1> P_CG);
+Matrix<float, 4, 4> transform_matrix(Matrix<float, 3, 3> R, Matrix<float, 3, 1> P);
+Matrix<float, 4, 4> transform_matrix(float R[], float P[]);
 
 }  // namespace src::utils::MatrixHelper
