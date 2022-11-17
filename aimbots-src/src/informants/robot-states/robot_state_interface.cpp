a30daf3c4114345fee5425312ab25b8392a739c4
jonathan <jonathan@postelnik.net>
2022-11-16 19:25:57 -0600

    .

--- aimbots-src/src/informants/robot-states/robot_state_interface.cpp
+++ aimbots-src/src/informants/robot-states/robot_state_interface.cpp
@@ -1,5 +1,5 @@
 #include "drivers.hpp"
-
+#include "ref_serial.hpp"
 #include "robot_state.hpp"
 #include "robot_states_interface.hpp"
 
@@ -7,24 +7,24 @@ namespace src::robotStates {
 
 RobotStates::RobotStates(src::Drivers* drivers) : drivers(drivers) {
     robotStates = Matrix<Robot, 2, 9>().zeroMatrix();
-    robotStates[Team::RED][0] = Robot(Matrix<float,3,1>().zeroMatrix(), 1, 0, Team::RED);  // HERO
-    robotStates[Team::RED][1] = Robot(Matrix<float,3,1>().zeroMatrix(), 2, 0, Team::RED);  // ENGINEER
-    robotStates[Team::RED][2] = Robot(Matrix<float,3,1>().zeroMatrix(), 3, 0, Team::RED);  // STANDARD
-    robotStates[Team::RED][3] = Robot(Matrix<float,3,1>().zeroMatrix(), 4, 0, Team::RED);  // STANDARD
-    robotStates[Team::RED][4] = Robot(Matrix<float,3,1>().zeroMatrix(), 5, 0, Team::RED);  // STANDARD
-    robotStates[Team::RED][5] = Robot(Matrix<float,3,1>().zeroMatrix(), 6, 0, Team::RED);  // Aerial
-    robotStates[Team::RED][6] = Robot(Matrix<float,3,1>().zeroMatrix(), 7, 0, Team::RED);  // sentry
-    robotStates[Team::RED][7] = Robot(Matrix<float,3,1>().zeroMatrix(), 8, 0, Team::RED);  //  BASE
-    robotStates[Team::RED][8] = Robot(Matrix<float,3,1>().zeroMatrix(), 9, 0, Team::RED);  // OUTPOST
-    robotStates[Team::BLUE][0] = Robot(Matrix<float,3,1>().zeroMatrix(), 1, 0, Team::BLUE);
-    robotStates[Team::BLUE][1] = Robot(Matrix<float,3,1>().zeroMatrix(), 2, 0, Team::BLUE);
-    robotStates[Team::BLUE][2] = Robot(Matrix<float,3,1>().zeroMatrix(), 3, 0, Team::BLUE);
-    robotStates[Team::BLUE][3] = Robot(Matrix<float,3,1>().zeroMatrix(), 4, 0, Team::BLUE);
-    robotStates[Team::BLUE][4] = Robot(Matrix<float,3,1>().zeroMatrix(), 5, 0, Team::BLUE);
-    robotStates[Team::BLUE][5] = Robot(Matrix<float,3,1>().zeroMatrix(), 6, 0, Team::BLUE);
-    robotStates[Team::BLUE][6] = Robot(Matrix<float,3,1>().zeroMatrix(), 7, 0, Team::BLUE);
-    robotStates[Team::BLUE][7] = Robot(Matrix<float,3,1>().zeroMatrix(), 8, 0, Team::BLUE);
-    robotStates[Team::BLUE][8] = Robot(Matrix<float,3,1>().zeroMatrix(), 9, 0, Team::BLUE);
+    robotStates[Team::RED][0] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 1, 0, Team::RED);  // HERO
+    robotStates[Team::RED][1] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 2, 0, Team::RED);  // ENGINEER
+    robotStates[Team::RED][2] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 3, 0, Team::RED);  // STANDARD
+    robotStates[Team::RED][3] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 4, 0, Team::RED);  // STANDARD
+    robotStates[Team::RED][4] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 5, 0, Team::RED);  // STANDARD
+    robotStates[Team::RED][5] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 6, 0, Team::RED);  // Aerial
+    robotStates[Team::RED][6] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 7, 0, Team::RED);  // sentry
+    robotStates[Team::RED][7] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 8, 0, Team::RED);  //  BASE
+    robotStates[Team::RED][8] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 9, 0, Team::RED);  // OUTPOST
+    robotStates[Team::BLUE][0] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 1, 0, Team::BLUE);
+    robotStates[Team::BLUE][1] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 2, 0, Team::BLUE);
+    robotStates[Team::BLUE][2] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 3, 0, Team::BLUE);
+    robotStates[Team::BLUE][3] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 4, 0, Team::BLUE);
+    robotStates[Team::BLUE][4] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 5, 0, Team::BLUE);
+    robotStates[Team::BLUE][5] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 6, 0, Team::BLUE);
+    robotStates[Team::BLUE][6] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 7, 0, Team::BLUE);
+    robotStates[Team::BLUE][7] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 8, 0, Team::BLUE);
+    robotStates[Team::BLUE][8] = Robot(Matrix<float, 3, 1>().zeroMatrix(), 9, 0, Team::BLUE);
 }
 
 RobotStates::~RobotStates() {}
@@ -47,9 +47,11 @@ void RobotStates::updateRobotStatePosition(int number, Team teamColor, float x, 
 void RobotStates::updateRobotStateHealth(int number, Team teamColor, int health) { robotStates[teamColor][number - 1].setHealth(health); }
 
 void updateRefSystem() {
-    // if (driver->refSerial.getSerialReceivingData()) {
-    //     driver->refSerial.getRobotDate().RobotData.
-    // }
+    if (driver->refSerial.getRefSerialReceivingData()) {
+        driver->refSerial.messageReceiveCallback();
+        uint8_t id = driver->refSerial.getRobotData().RobotData.robotId;
+        uint16_t hp = driver->refSerial.getRobotData().RobotData.currentHp;
+    }
 }
 
 }  // namespace src::robotStates
