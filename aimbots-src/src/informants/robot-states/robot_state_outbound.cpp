9e13697442fa5f06a3deb055988582b4ce1603d5
jonathan <jonathan@postelnik.net>
2022-11-27 17:35:31 -0600

    more work done over the break. more needs to be writen

--- aimbots-src/src/informants/robot-states/robot_state_outbound.cpp
+++ aimbots-src/src/informants/robot-states/robot_state_outbound.cpp
@@ -6,12 +6,24 @@
 #include "drivers.hpp"
 #include "robot_state.hpp"
 #include "robot_state_interface.hpp"
-
+#include "robot_state_message.hpp"
 
 namespace src::robotStates {
 RobotStateOutBound(src::Drivers* drivers) : drivers(drivers), refSerial(drivers) {}
 
 bool send() { return false; }
 
-void updateQue() {}
+void updateQue() {
+#ifdef TARGET_SENTRY
+    Team color = drivers->getRobotData->robotData.robotId == RED_SENTINEL ? Team::RED : Team::BLUE;
+    Team colorEnemy = color == TEAM::RED ? TEAM::BLUE : TEAM::RED;
+
+    #ifdef TARGET_RUML
+
+    
+    #endif
+#else
+
+#endif
+}
 }  // namespace src::robotStates
