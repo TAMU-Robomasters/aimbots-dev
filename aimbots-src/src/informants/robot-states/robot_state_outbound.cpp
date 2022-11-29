a26b85cac74df85477f893ff1199672f10e1c641
jonathan <jonathan@postelnik.net>
2022-11-28 21:51:07 -0600

    i wrote some code. add more to the 'robot_state_outbound/cpp' current issue is that it does not really work or any where close to a working state. will need to add a lot to update que and figure out how I am going to do it for sentry and then convert it to non sentry robots. a lot of work needs to be done

--- aimbots-src/src/informants/robot-states/robot_state_outbound.cpp
+++ aimbots-src/src/informants/robot-states/robot_state_outbound.cpp
@@ -11,17 +11,28 @@
 namespace src::robotStates {
 RobotStateOutBound(src::Drivers* drivers) : drivers(drivers), refSerial(drivers) {}
 
-bool send() { return false; }
+bool send() {
+    updateQue();
+
+    PT_CALL(refSerial.sendRobotToRobotMsg(
+        &robotToRobotMessage,
+        SENTRY_REQUEST_ROBOT_ID,
+        drivers->refSerial.getRobotIdBasedOnCurrentRobotTeam(RefSerialData::RobotId::BLUE_SENTINEL),
+        1));
+
+    PT_END();
+    return false;
+}
 
 void updateQue() {
 #ifdef TARGET_SENTRY
     Team color = drivers->getRobotData->robotData.robotId == RED_SENTINEL ? Team::RED : Team::BLUE;
     Team colorEnemy = color == TEAM::RED ? TEAM::BLUE : TEAM::RED;
 
-    #ifdef TARGET_RUML
-
     
-    #endif
+#ifdef TARGET_RUML
+
+#endif
 #else
 
 #endif
--- aimbots-src/src/informants/robot-states/robot_state_outbound.hpp
+++ aimbots-src/src/informants/robot-states/robot_state_outbound.hpp
@@ -2,9 +2,9 @@
 
 #include "modm/architecture/interface/register.hpp"
 #include "modm/processing/protothread.hpp"
+#include "src/communication/serial/ref_serial.hpp"
 
 #include "drivers.hpp"
-#include "ref_serial.hpp"
 #include "robot_state.hpp"
 #include "robot_state_interface.hpp"
 
@@ -14,16 +14,22 @@ class RobotStateOutBound : public modm::pt::Protothread {
 private:
     src::Drivers* drivers;
     tap::communication::serial::RefSerialTransmitter refSerial;
+    tap::communication::serial::RefSerialData::Tx::RobotToRobotMessage robotToRobotMessage;
 
     RobotStateInterface* robotStateInterface;
 
+    uint_16 message{};
+
+// #ifdef TARGET_SENTRY
+    static constexpr uint16_t SENTRY_REQUEST_ROBOT_ID = 0x200;
+
+// #endif
 public:
     RobotStateOutBound(src::Drivers* drivers);
 
     bool send();
 
     void updateQue();
-
 }
 
 }  // namespace src::robotStates
