2338c88a7513815398df3461327bfa615faaa35c
David Sun <davids.atx1@gmail.com>
2023-03-01 21:27:01 -0600

    added this function that was in gimbal but it does not seem to be necessary here

--- aimbots-src/src/subsystems/shooter/barrel_swap/swap_mechanism.cpp
+++ aimbots-src/src/subsystems/shooter/barrel_swap/swap_mechanism.cpp
@@ -5,7 +5,8 @@ namespace src::Shooter{
     SwapMechanismSubsystem::SwapMechanismSubsystem(src::Drivers* drivers)
     : Subsystem(drivers),
     swapMotor(drivers, SWAP_MOTOR_ID, GIMBAL_BUS, SWAP_DIRECTION),
-    currentSwapMotorRelativeAngle(0.0f, 0.0f, M_TWOPI)
+    currentSwapMotorRelativeAngle(0.0f, 0.0f, M_TWOPI),
+    targetSwapMotorRelativeAngle(modm::toRadian(SWAP_START_ANGLE)) {}
 
     void SwapMechanismSubsystem::initialize() {
         swapMotor.initialize();
@@ -36,6 +37,16 @@ namespace src::Shooter{
         // m2006 motor
         // this function modified from gimbal.cpp:129
         desiredSwapMotorOutput = tap::algorithms::limitVal(output, -M2006_MAX_OUTPUT, M2006_MAX_OUTPUT);
+    }
+
+    float SwapMechanismSubsystem::getCurrentAngleFromChassisCenter(AngleUnit unit) const {
+        // this function modified from gimbal.cpp:145
+        return tap::algorithms::ContiguousFloat(
+                (unit == AngleUnit::Degrees) ? modm::toDegree(currentSwapMotorRelativeAngle.getValue() - modm::toRadian(SWAP_START_ANGLE)) : (currentSwapMotorRelativeAngle.getValue() - modm::toRadian(SWAP_START_ANGLE)),
+                (unit == AngleUnit::Degrees) ? -180.0f : -M_PI,
+                (unit == AngleUnit::Degrees) ? 180.0f : M_PI)
+            .getValue();
+        )
     }
 
 }
