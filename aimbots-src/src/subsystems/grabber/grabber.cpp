a2f71b251ff9c2b83d023c490ca2c65a23d04ab1
HeroOfWind243 <67348400+HeroOfWind243@users.noreply.github.com>
2023-11-13 20:49:36 -0600

    Skeleton subsystem started

---
+++ aimbots-src/src/subsystems/grabber/grabber.cpp
@@ -0,0 +1,14 @@
+#include "grabber.hpp"
+
+#ifdef GRABBER_COMPATIBLE //TODO: Define this in a constants file later
+
+namespace src::Grabber {
+
+
+
+
+
+} //namespace src::Grabber
+
+
+#endif //GRABBER_COMPATIBLE
