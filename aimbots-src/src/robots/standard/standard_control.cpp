527dce6c5273a9d0e8bd869dfd5bb4ceff9c5d58
jpostelnik <jonathan@postelnik.net>
2024-01-22 19:56:04 -0600

    safe to merge

--- aimbots-src/src/robots/standard/standard_control.cpp
+++ aimbots-src/src/robots/standard/standard_control.cpp
@@ -357,8 +357,8 @@ void startupCommands(src::Drivers *drivers) {
 
 // Register IO mappings here -----------------------------------------------
 void registerIOMappings(src::Drivers *drivers) {
-    // drivers->commandMapper.addMap(&leftSwitchUp);
-    // drivers->commandMapper.addMap(&leftSwitchMid);
+    drivers->commandMapper.addMap(&leftSwitchUp);
+    drivers->commandMapper.addMap(&leftSwitchMid);
     drivers->commandMapper.addMap(&rightSwitchUp);
     drivers->commandMapper.addMap(&rightSwitchMid);
     drivers->commandMapper.addMap(&rightSwitchDown);
