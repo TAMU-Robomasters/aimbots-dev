8d2c1925a22985e394f36c475371804c36f25c71
tylerhedge <tylerhedge1@gmail.com>
2024-01-31 21:00:55 -0600

    Good progress can almost compile!!!
    I'll compile it for a scooby snack.
    
    There is a dead rat in the deep fryer, remy isnt cooking any more gang. Rest in power.

--- aimbots-src/src/informants/pathfinding/graph.hpp
+++ aimbots-src/src/informants/pathfinding/graph.hpp
@@ -2,9 +2,6 @@
 #include <unordered_map>
 #include "utils/common_types.hpp"
 #include <cmath>
-#include <vector>
-
-using namespace std;
 
 //I stole all of this because I have not taken DSA
 //https://www.redblobgames.com/pathfinding/a-star/implementation.html#cpp
@@ -12,38 +9,32 @@ using namespace std;
 //represents a location in a graph as apposed to an ID, because I think this is more scalable, this may be a bad idea
 
 
-struct Graph_Loc { 
-    int x;
-    int y;
-};
-
 struct WeightedSquareGraph {
-    unordered_map<Graph_Loc, Graph_Loc> edges;
+    std::unordered_map<modm::Vector2i, modm::Vector2i> edges;
 
-    Graph_Loc* get_neighbors(Graph_Loc loc) {
-        // Generates 8 neighbors on the grid
-        //   4 orthogonal & 4 diagonal, similar to Minesweeper
-        vector<Graph_Loc> neighbors;
-        int x = loc.x;
-        int y = loc.y;
+    // He was a modm::Vector2i. She was a Graph_loc. Will they fall in love?
+
+    void get_neighbors(modm::Vector2i loc, std::array<modm::Vector2i, 8>* neighbors) {
+        // Generates up all neighbors on the grid relative to a specific node
+        //   Up to 4 orthogonal & 4 diagonal, similar to Minesweeper
         int k = 0;
         for (int i = -1; i < 2; i++){
             for (int j = -1; j <2; j++){
                 if (j == 0 && i == 0) {continue;}
-                Graph_Loc neighbor;
-                neighbor.x = i + x;
-                neighbor.y = j + y;
-                neighbors[k] = neighbor;
+                modm::Vector2i neighbor;
+                neighbor[0] = i + loc[0];
+                neighbor[1] = j + loc[1];
+                *neighbors + k = neighbor;
+                
                 k++;
             }
         }
-            return &neighbors;
         }
     
     // Returns the distance between two nodes
-    double get_cost(Graph_loc from_node, Graph_Loc to_node){
-        return std::sqrt(pow2(to_node.x - from_node.x) + pow2(to_node.y - from_node.y));
+   /* double get_cost(Vector<> from_node, Graph_Loc to_node){
+        return std::sqrt(pow2(to_node.x - from_node.x) + pow2(to_node.y - from_node.y)); // tengo bomba :steam_happy:
     }
-        
+    */
 
     }; // boo
