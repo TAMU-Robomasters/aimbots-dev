c0302eda6d0f9f8a99f9459eeb22ed1091779adc
nsumma <quitguitar@gmail.com>
2024-02-07 20:46:51 -0600

    messing around with the kalman alg
    Co-Authored-By: Zilin Zhao <43014160+Jucedous@users.noreply.github.com>

--- aimbots-src/src/utils/kinematic_state_vector.hpp
+++ aimbots-src/src/utils/kinematic_state_vector.hpp
@@ -4,15 +4,21 @@
 
 #include "utils/common_types.hpp"
 
+#include "taproot/src\tap/algorithms/extended_kalman.hpp"
+
 namespace src::Utils {
 class KinematicStateVector {
 public:
     KinematicStateVector()
         : stateVector(Vector3f(0, 0, 0)),
-          lastTime(0)  //
+          lastTime(0),
+          lastA(0)  //
     {}
     ~KinematicStateVector() = default;
 
+    ExtendedKalmanKSV(float tq, float tR)
+    float filterData(float dat);
+    
     Vector3f getStateVector() { return stateVector; }
 
     inline float getPosition() { return stateVector.getX(); }
@@ -51,12 +57,59 @@ public:
 
         lastTime = currTime;
     }
+
+
+    float xLast;  ///< last optimal prediction.
+    float xMid;   ///< forcast optimal prediction.
+    float xNow;   ///< current optimal prediction.
+    float pMid;   ///< predicted covariance.
+    float pNow;   ///< current covariance.
+    float pLast;  ///< previous covariance.
+    float kg;     ///< kalman gain.
+    float A;      ///< system parameter.
+    float B;      ///< system parameter.
+    float Q;      ///< system parameter
+    float R;      ///< system parameter.
+    float H;      ///< system parameter.
+    
+    ExtendedKalman::ExtendedKalman(float tQ, float tR)
+    : xLast(0.0f),
+      xMid(0.0f),
+      xNow(0.0f),
+      pMid(0.0f),
+      pNow(0.0f),
+      pLast(0.0f),
+      kg(0.0f),
+      A(1.0f),
+      B(0.0f),
+      Q(tQ),
+      R(tR),
+      H(1.0f)
+    
+    float filterData(float dat)
+    {
+        xMid = A * xLast;
+        pMid = A * pLast + Q;
+        kg = pMid / (pMid + R);
+        xNow = xMid + kg * (dat - xMid);
+        pNow = (1 - kg) * pMid;
+        pLast = pNow;
+        xLast = xNow;
+        return xNow;
+    }
+    ExtendedKalman kalman(1.0f,0.0f); //percents that do predictions and updates
+
     float dTime = 0;
     float V = 0;
     float S = 0;
     float count = 0;
+    float filteredA = 0;
     void updateFromAcceleration(float a, bool updateLowerOrderTerms = true, std::optional<float> dt = std::nullopt) {
         float currTime = tap::arch::clock::getTimeMilliseconds();
+        if (abs(a - lastA)/(currTime - lastTime) > 10) {
+            lastTime = currTime;
+            return;
+        }
         if (updateLowerOrderTerms) {
             // float v1 = calcIntegral(getVelocity(), a, dt.value_or(currTime - lastTime));
             // float x1 = calcIntegral(getPosition(), v1, dt.value_or(currTime - lastTime));
@@ -69,6 +122,7 @@ public:
             } else {
                 a = 0;
             }
+            filteredA = kalman.filterData(a);
             setPosition(v2);
             //float x1 = getPosition() + 2;
             // float x1 = getPosition() + (v1) / 2 * (currTime - lastTime);
@@ -76,8 +130,8 @@ public:
             //float x1 = currTime;
             //setPosition(x1);
         }
-        setAcceleration(a);
-
+        setAcceleration(filteredA);
+        lastA = filteredA;
         lastTime = currTime;
     }
 
@@ -113,5 +167,6 @@ private:
 
     Vector3f stateVector;
     uint32_t lastTime;
+    uint32_t lastA;
 };
 }  // namespace src::Utils
