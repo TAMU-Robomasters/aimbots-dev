00ce479a7589cd5f533917e34b4b3ef1f8697415
Noe <alejolaverde0927@gmail.com>
2024-02-12 19:14:56 -0600

    Try cleaning the grabber code before testing it later

--- aimbots-src/src/subsystems/grabber/grabber.cpp
+++ aimbots-src/src/subsystems/grabber/grabber.cpp
@@ -6,6 +6,10 @@
 #include "drivers.hpp"
 
 #ifdef GRABBER_COMPATIBLE
+
+#define ON 1
+#define OFF 0
+
 /**
  * Potential problem1: on testing, the trigger acting the same to duty cycle = 1 when setting to 0.5. Need more testing on behavior on dudy circle change.
  * Potential problem2: unknown effect on frequency difference in GrabberSubsystem::initialize()
@@ -13,47 +17,37 @@
 namespace src::Grabber {
 
 GrabberSubsystem::GrabberSubsystem(tap::Drivers* drivers)
-    : Subsystem(drivers)
+    : Subsystem(drivers), pwm(&drivers->pwm)
 {
-
 }
+
 int account_entering_commands;
-/**
- * initialize local frequency
- */
+
 void GrabberSubsystem::initialize() {
-    drivers->pwm.init();
-    drivers->pwm.setTimerFrequency(tap::gpio::Pwm::Timer::TIMER1 ,2000);
+    pwm->init();
 }
 
 void GrabberSubsystem::refresh() {
 }
 
-/**
- * set duty cycle to 0, deactivate the trigger
- */
 void GrabberSubsystem::deactivate() {
-    drivers->pwm.write(0.0f, GRABBER_PIN);
+    pwm->write(OFF, GRABBER_PIN);
 }
 
-
-/**
- * set duty cycle to 1, activate the trigger
- */
-void GrabberSubsystem::activate(){
-    drivers->pwm.write(1.0f, GRABBER_PIN);
+void GrabberSubsystem::activate() {
+    pwm->write(ON, GRABBER_PIN);
 }
 
 /**
  * testing for different behavior on different duty cycle
  */
-void GrabberSubsystem::unknown(){
+void GrabberSubsystem::unknown() {
     // while(true) {
     //     drivers->pwm.write(1.0f, GRABBER_PIN);
     //     drivers->pwm.write(0.0f, GRABBER_PIN);
     // }
-    drivers->pwm.write(1.0f, GRABBER_PIN);
-    drivers->pwm.write(0.0f, GRABBER_PIN);
+    pwm->write(1.0f, GRABBER_PIN);
+    pwm->write(0.0f, GRABBER_PIN);
 }
 
 
