2f7ad62eacd1342cb57f495743d2d3dcd673730a
Baconsizzle1738 <zhengdundun@gmail.com>
2024-02-28 19:53:31 -0600

    livesare died :( also finished implementation of functions

--- aimbots-src/src/utils/math/transformations.cpp
+++ aimbots-src/src/utils/math/transformations.cpp
@@ -1,14 +1,27 @@
 #include "utils/common_types.hpp"
 #include "utils/math/matrix_helpers.hpp"
 
-using namespace src::Utils::MatrixHelper;// for matrix helper functions
+using namespace src::Utils::MatrixHelper; // for matrix helper functions
 
 namespace src::Utils::MatrixOperations {
 
+    Matrix3f identityMatrix(){
+        float transform_array[9] = {1.0f, 0.0f, 0.0f,
+                                    0.0f, 1.0f, 0.0f,
+                                    0.0f, 0.0f, 1.0f};
+    }
 
-    Vector3f rotate(Vector3f M, float theta, float phi) {
 
-        return rotationMatrix(theta, LinearAxis::X_AXIS, AngleUnit::Radians) * rotationMatrix(phi, LinearAxis::Y_AXIS, AngleUnit::Radians) * M;
+    Vector3f rotate(Vector3f v, float theta, float phi) {
+
+        return rotationMatrix(theta, LinearAxis::X_AXIS, AngleUnit::Radians) * rotationMatrix(phi, LinearAxis::Y_AXIS, AngleUnit::Radians) * v;
+
+    }
+
+    Vector3f translate(Vector3f v, Vector3f p) {
+
+        float translatedMatrix[3] = {v[0] + p[0], v[1] + p[1], v[2] + p[2]};
+        return Vector3f(translatedMatrix);
 
     }
 
