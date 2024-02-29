a4680e8d36c283798e8290137b601aa9c50c89bd
Baconsizzle1738 <zhengdundun@gmail.com>
2024-02-28 21:00:30 -0600

    Finished transformations and attempting new frames math

--- aimbots-src/src/utils/math/transformations.cpp
+++ aimbots-src/src/utils/math/transformations.cpp
@@ -6,23 +6,20 @@ using namespace src::Utils::MatrixHelper; // for matrix helper functions
 namespace src::Utils::MatrixOperations {
 
     Matrix3f identityMatrix(){
-        float transform_array[9] = {1.0f, 0.0f, 0.0f,
-                                    0.0f, 1.0f, 0.0f,
-                                    0.0f, 0.0f, 1.0f};
+        float identity_array[9] = {1.0f, 0.0f, 0.0f,
+                                   0.0f, 1.0f, 0.0f,
+                                   0.0f, 0.0f, 1.0f};
+        return Matrix3f(identity_array);            
     }
 
-
     Vector3f rotate(Vector3f v, float theta, float phi) {
-
         return rotationMatrix(theta, LinearAxis::X_AXIS, AngleUnit::Radians) * rotationMatrix(phi, LinearAxis::Y_AXIS, AngleUnit::Radians) * v;
-
     }
 
     Vector3f translate(Vector3f v, Vector3f p) {
-
-        float translatedMatrix[3] = {v[0] + p[0], v[1] + p[1], v[2] + p[2]};
-        return Vector3f(translatedMatrix);
-
+        // float translatedMatrix[3] = {v[0] + p[0], v[1] + p[1], v[2] + p[2]};
+        // return Vector3f(translatedMatrix);
+        return v + p;
     }
 
 }
--- aimbots-src/src/utils/math/transformations.hpp
+++ aimbots-src/src/utils/math/transformations.hpp
@@ -3,23 +3,25 @@
 #include "utils/common_types.hpp"
 
 namespace src::Utils::MatrixOperations {
-    /*
-    * Creates a 3x3 identity matrix in identity form
-    */
+    /**
+    * @return   3X3 identity matrix
+    **/
     Matrix3f identityMatrix();
 
-    /*
+    /**
     * Rotate the matrix
     * @param v      Vector to rotate
     * @param theta  angle to rotate about the x axis, in radians
     * @param phi    angle to roate about the y axis, in radians
-    */
+    * @return       rotated column vector
+    **/
     Vector3f rotate(Vector3f v, float theta, float phi);
 
-    /*
+    /**
     * Translate the matrix
     * @param v  Vector to transform
     * @param p  Translation vector
-    */
+    * @return   Translated column vector
+    **/
     Vector3f translate(Vector3f v, Vector3f p);
 }
