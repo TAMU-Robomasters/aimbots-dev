ef5a0750cb845847f3e283d0e9c43871147a1423
zDagger <jdagcarroll@gmail.com>
2024-03-20 20:36:37 -0500

    got hit directon detection informant working, needs ti be called and then tested

---
+++ aimbots-src/src/informants/hitTracker.cpp
@@ -0,0 +1,49 @@
+#include "hitTracker.hpp"
+#include "subsystems/gimbal/gimbal.hpp"
+#include "utils/common_types.hpp"
+#include "drivers.hpp"
+
+namespace src::Informants {
+    
+HitTracker::HitTracker(src::Drivers* drivers)
+    : drivers(drivers)
+{
+}
+
+
+bool HitTracker::wasHit(){
+    return (getPrevHp() > getCurrHP());
+}
+
+float HitTracker::getHitAngle_chassisRelative(){
+    //get armor panel hit
+    RefSerialRxData::ArmorId panel = getHitPanelID();
+    float hitAngle = 0.0f;
+    switch(panel) {
+        case RefSerialRxData::ArmorId::FRONT:
+            hitAngle = 0.0f;
+            break;
+        case RefSerialRxData::ArmorId::LEFT:
+            hitAngle = M_PI / 2.0f;
+            break;
+        case RefSerialRxData::ArmorId::REAR:
+            hitAngle = M_PI;
+            break;
+        case RefSerialRxData::ArmorId::RIGHT:
+            hitAngle = -M_PI / 2.0f;
+            break;
+        default:
+            break;
+    }
+    return hitAngle;
+}
+
+float HitTracker::getHitAngle_gimbalRelative(){
+    //get chassisRelative angle?
+    float chassis_hitAngle = getHitAngle_chassisRelative();
+    //get angle btwn gimbal-chassis?
+    float gimbalAngle = gimbal->getCurrentYawAxisAngle(AngleUnit::Radians);
+    //calc and return
+    return gimbalAngle + chassis_hitAngle;
+}
+}
--- aimbots-src/src/informants/hitTracker.hpp
+++ aimbots-src/src/informants/hitTracker.hpp
@@ -1,2 +1,49 @@
 //should be object that acesses robot data through
 //ref helper and gets updated at driver level
+#pragma once
+
+#include "utils/common_types.hpp"
+#include "subsystems/gimbal/gimbal.hpp"
+
+#include "drivers.hpp"
+
+using RefSerialRxData = tap::communication::serial::RefSerialData::Rx;
+
+
+namespace src::Informants {
+class HitTracker{
+public:
+    HitTracker(src::Drivers* drivers);
+    HitTracker() = default;
+    RefSerialRxData::ArmorId getHitPanelID() {
+        auto robotData = drivers->refSerial.getRobotData();
+        return robotData.damagedArmorId;
+    }
+    uint16_t getPrevHp(){
+        auto robotData = drivers->refSerial.getRobotData();
+        return robotData.previousHp;
+    }
+    uint16_t getCurrHP(){
+        auto robotData = drivers->refSerial.getRobotData();
+        return robotData.currentHp;
+    }
+    uint32_t getDataTimeStamp(){
+        auto robotData = drivers->refSerial.getRobotData();
+        return robotData.robotDataReceivedTimestamp;
+    }
+
+    bool wasHit();
+
+    //returns hit angle relative to chassis front as 0
+    float getHitAngle_chassisRelative();
+
+    //returns hit angle relative to gimbal front as 0
+    float getHitAngle_gimbalRelative();
+
+
+    private:
+    src::Drivers* drivers;
+    src::Gimbal::GimbalSubsystem* gimbal;
+};
+}
+
