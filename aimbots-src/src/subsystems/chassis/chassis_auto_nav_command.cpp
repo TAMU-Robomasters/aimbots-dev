04f25c9a7e943004f676b13b0769c251eb61f992
tylerhedge <tylerhedge1@gmail.com>
2024-04-01 20:55:00 -0500

    more debugging stuff added, fixed atleast 1 bug, removed turning

--- aimbots-src/src/subsystems/chassis/chassis_auto_nav_command.cpp
+++ aimbots-src/src/subsystems/chassis/chassis_auto_nav_command.cpp
@@ -27,19 +27,21 @@ ChassisAutoNavCommand::ChassisAutoNavCommand(
 
 float path_x = 0.0f;
 float path_y = 0.0f;
+int A = 0;
 
 void ChassisAutoNavCommand::initialize() {
     // modm::Location2D<float> targetLocation({0.5f, 0.5f}, modm::toRadian(90.0f));  // test
     // autoNavigator.setTargetLocation(targetLocation);
     WeightedSquareGraph graph = WeightedSquareGraph(1, 1, 0.1);
-    this->load_path(graph.get_path(Vector2i(0,0), Vector2i(1, 1)));
+    this->load_path(std::vector<Vector2f> {Vector2f(0, 0.1), Vector2f(0.2, 0.2), Vector2f(0.2, 0), Vector2f(0, 0)});
 }
 
 void ChassisAutoNavCommand::pop_path(){
+        if (path.empty()) { return; }
         Vector2f pt = path.front();
         path_x = pt.x;
         path_y = pt.y;
-        autoNavigator.setTargetLocation(modm::Location2D<float>(pt.getX(), pt.getY(), 90));
+        autoNavigator.setTargetLocation(modm::Location2D<float>(pt.getX(), pt.getY(), 0));
         path.erase(path.begin());
     }
 
@@ -56,14 +58,17 @@ void ChassisAutoNavCommand::execute() {
     float xError = 0.0f;
     float yError = 0.0f;
     float rotationError = 0.0f;
+    A++;
 
     //no points to load and controllers at target
     if (this->path.empty() && this->isSettled()){
+        chassis->setTargetRPMs(0.0f, 0.0f, 0.0f);
         return; 
     }
 
-    if (this->isSettled()){ //if controllers at target load new point
+    if (this->isSettled() && A > 100){ //if controllers at target load new point
         this->pop_path();
+        A = 0;
     }
     
     modm::Location2D<float> currentWorldLocation = drivers->kinematicInformant.getRobotLocation2D();
@@ -79,11 +84,12 @@ void ChassisAutoNavCommand::execute() {
 
     float desiredX = xController.runController(xError, currentWorldVelocity.getX());
     float desiredY = yController.runController(yError, currentWorldVelocity.getY());
-    float desiredR = rotationController.runController(
-        rotationError,
-        -RADPS_TO_RPM(drivers->kinematicInformant.getChassisIMUAngularVelocity(
-            src::Informants::AngularAxis::YAW_AXIS,
-            AngleUnit::Radians)));
+    float desiredR = 0; 
+    // float desiredR = rotationController.runController(
+    //     rotationError,
+    //     -RADPS_TO_RPM(drivers->kinematicInformant.getChassisIMUAngularVelocity(
+    //         src::Informants::AngularAxis::YAW_AXIS,
+    //         AngleUnit::Radians)));
 
     // Rotate world-relative desired input to chassis-relative desired input
     tap::algorithms::rotateVector(&desiredX, &desiredY, -currentWorldLocation.getOrientation());
