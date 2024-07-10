1ae4936e772849429949a2074aac09e32e8434cb
Nathaniel Speed <speed.nathaniel@outlook.com>
2024-07-10 14:03:20 -0500

    Fixed a skill issue with the standard constants for Blastoise

--- aimbots-src/src/robots/standard/standard_constants.hpp
+++ aimbots-src/src/robots/standard/standard_constants.hpp
@@ -33,7 +33,7 @@ static constexpr uint8_t FEEDER_MOTOR_COUNT = 1;
 
 #ifdef TARGET_STANDARD_BLASTOISE
 /* What motor angles ensures that the barrel is pointing straight forward and level relative to the robot chassis? */
-// static const std::array<float, YAW_MOTOR_COUNT> YAW_MOTOR_OFFSET_ANGLES = {wrapTo0To2PIRange(modm::toRadian(0))};
+static const std::array<float, YAW_MOTOR_COUNT> YAW_MOTOR_OFFSET_ANGLES = {wrapTo0To2PIRange(modm::toRadian(0))};
 static const std::array<float, PITCH_MOTOR_COUNT> PITCH_MOTOR_OFFSET_ANGLES = {modm::toRadian(-5.23f)};
 static constexpr float PITCH_AXIS_SOFTSTOP_LOW = modm::toRadian(-14.5f);
 static constexpr float PITCH_AXIS_SOFTSTOP_HIGH = modm::toRadian(17.5f);
