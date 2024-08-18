7f48ce9359e42ee7efbffe64ca786dc965f4a438
n-speed <speed.nathaniel@outlook.com>
2024-08-17 23:47:01 -0500

    Revert "Jetson Comms rewrite still WIP. Deciphering updateSerial()" This reverts commit a6707fe0f6ae1e6da7427e6f06a9348ab9f63988.

--- aimbots-src/src/informants/vision/jetson_communicator.hpp
+++ aimbots-src/src/informants/vision/jetson_communicator.hpp
@@ -1,16 +1,10 @@
 #pragma once
 
-#include <cassert>
-#include <deque>
-
 #include <tap/architecture/timeout.hpp>
 #include <tap/architecture/periodic_timer.hpp>
 #include <tap/util_macros.hpp>
-#include <tap/communication/serial/dji_serial.hpp>
-#include <tap/communication/serial/ref_serial_data.hpp>
-#include <tap/drivers.hpp>
+#include <utils/common_types.hpp>
 
-#include "utils/common_types.hpp"
 #include "informants/enemy_data_conversion.hpp"
 
 namespace src {
@@ -19,47 +13,40 @@ class Drivers;
 
 namespace src::Informants::Vision {
 
+enum class JetsonCommunicatorSerialState : uint8_t {
+    SearchingForMagic = 0,
+    AssemblingMessage,
+};
+
 enum CVState : uint8_t {
     NOT_FOUND = 0,
     FOUND = 1,
     FIRE = 2,
 };
 
-class JetsonCommunicator : public tap::communication::serial::DJISerial {
+static constexpr uint8_t JETSON_MESSAGE_MAGIC = 'a';
+
+struct JetsonMessage {
+    uint8_t magic;
+    float targetX;
+    float targetY;
+    float targetZ;
+    uint8_t delay;  // ms
+    CVState cvState;
+} __attribute__((packed));
+
+static_assert(sizeof(JetsonMessage) == 15, "JetsonMessage is not the correct size");
+
+static constexpr size_t JETSON_MESSAGE_SIZE = sizeof(JetsonMessage);
+
+class JetsonCommunicator {
     public:
-
-        static constexpr uint32_t JETSON_BAUD_RATE = 115200;
-
-        static constexpr UartPort JETSON_UART_PORT = UartPort::Uart1;
-        struct JetsonMessage {
-            float targetX;
-            float targetY;
-            float targetZ;
-            uint8_t delay;  // ms
-            CVState cvState;
-        } modm_packed;
-
         JetsonCommunicator(src::Drivers* drivers);
         DISALLOW_COPY_AND_ASSIGN(JetsonCommunicator);
         ~JetsonCommunicator() = default;
 
         void initialize();
-
-        void messageReceiveCallback(const ReceivedSerialMessage& completeMessage) override;
-
-        void parseRxMessage();
-
         void updateSerial();
-
-        bool isLastFrameStale() const;
-
-        void sendMessage();
-
-        void sendRobotID();
-
-        void sendMatchInfo();
-
-        void sendMatchState();
 
         inline bool isJetsonOnline() const { return !jetsonOfflineTimeout.isExpired(); }
 
@@ -71,41 +58,26 @@ class JetsonCommunicator : public tap::communication::serial::DJISerial {
 
         inline uint32_t getLastFrameCaptureDelay() const { return visionDataConverter.getLastFrameCaptureDelay(); }
 
+        bool isLastFrameStale() const;
+
+        void sendRobotID();
+        void sendMatchTime();
+        void sendMatchState();
+
     private:
-        enum TXMessageTypes {
-            TX_MESSAGE_ROBOT_ID = 1,
-            TX_MESSAGE_MATCH_INFO = 2,
-            TX_MESSAGE_MATCH_STATE = 4
-        };
-
-        // Add more message types as CV does more cool shit
-        enum RXMessageTypes{
-            RX_TARGET_DATA = 1
-        };
-
         src::Drivers* drivers;
         src::Informants::Vision::VisionDataConversion visionDataConverter;
 
         alignas(JetsonMessage) uint8_t rawSerialBuffer[sizeof(JetsonMessage)];
 
-        // JetsonCommunicatorSerialState currentSerialState;
-        // size_t nextByteIndex;
-
-        static constexpr uint16_t JETSON_OFFLINE_TIMEOUT_MS = 2'000;
-
-        static constexpr uint32_t PERIOD_BTWN_TX_ROBOTID_MS = 3'000;
-
-        static constexpr uint32_t PERIOD_BTWN_TX_MATCH_INFO_MS = 1'000;
-
-        static constexpr uint32_t PERIOD_BTWN_TX_MATCH_RESULT_MS = 5'000;
+        JetsonCommunicatorSerialState currentSerialState;
+        size_t nextByteIndex;
 
         tap::arch::MilliTimeout jetsonOfflineTimeout;
 
-        tap::arch::PeriodicMilliTimer robotIdTimeoutTX{PERIOD_BTWN_TX_ROBOTID_MS};
-
-        tap::arch::PeriodicMilliTimer matchInfoTimeoutTX{PERIOD_BTWN_TX_MATCH_INFO_MS};
-
-        tap::arch::PeriodicMilliTimer matchResultTimeoutTX{PERIOD_BTWN_TX_MATCH_RESULT_MS};
+        static constexpr uint32_t JETSON_BAUD_RATE = 115200;
+        static constexpr uint16_t JETSON_OFFLINE_TIMEOUT_MILLISECONDS = 2000;
+        static constexpr UartPort JETSON_UART_PORT = UartPort::Uart1;
 
         JetsonMessage lastMessage;
         uint32_t lastFoundTargetTime;
@@ -118,5 +90,15 @@ class JetsonCommunicator : public tap::communication::serial::DJISerial {
         Matrix<float, 1, 2> visionTargetAngles;
         Vector3f visionTargetPosition;
 
+        enum TxMessageID
+        { 
+            TX_MESSAGE_REFEREE_MATCH_TIME = 0,
+            TX_MESSAGE_REFEERE_MATCH_RESULT = 1,
+            TX_MESSAGE_ROBOT_ID = 2,
+        };
+
+        static constexpr uint32_t TIME_ROBOTID_FREQ = 3'000;
+
+        tap::arch::PeriodicMilliTimer sendRobotIdTimeout{TIME_ROBOTID_FREQ};
     };
 }  // namespace src::Informants::Vision
