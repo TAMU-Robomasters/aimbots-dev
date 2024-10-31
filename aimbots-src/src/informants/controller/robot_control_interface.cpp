0c222fd7c067475978cbce11e2deda36c77cb8c0
HeroOfWind243 <67348400+HeroOfWind243@users.noreply.github.com>
2024-10-30 21:12:01 -0500

    Started generic operator interface, need to think of a good modular system

---
+++ aimbots-src/src/informants/controller/robot_control_interface.cpp
@@ -0,0 +1,273 @@
+#include "robot_control_interface.hpp"
+
+
+
+#include "utils/tools/robot_specific_defines.hpp"
+
+using namespace tap::communication::serial;
+using namespace tap::algorithms;
+
+float finalXWatch = 0;
+uint32_t timeCtr = 0;
+
+static constexpr float INPUT_X_INC = 0.003f;
+static constexpr float INPUT_Y_INC = 0.003f;
+static constexpr float INPUT_XY_STOP_INC = 0.03f;
+static constexpr float INPUT_R_INC = 0.003f;
+
+static constexpr float INPUT_WRIST_YAW_INC = 0.003f;
+static constexpr float INPUT_WRIST_PITCH_INC = 0.003f;
+static constexpr float INPUT_WRIST_ROLL_INC = 0.003f;
+
+static constexpr float INPUT_SLIDE_UPDOWN_INC = 0.006f;
+static constexpr float INPUT_SLIDE_FRONTBACK_INC = 0.006f;
+static constexpr float INPUT_SLIDE_STOP_INC = 0.06f;
+
+static constexpr float XAXIS_JOYSTICK_INPUT_SENSITIVITY = 0.0001;
+static constexpr float ZAXIS_JOYSTICK_INPUT_SENSITIVITY = 0.0001;
+
+static constexpr float YAW_JOYSTICK_INPUT_SENSITIVITY = 0.015f;
+static constexpr float PITCH_JOYSTICK_INPUT_SENSITIVITY = 0.015f;
+
+
+
+static constexpr float CTRL_SCALAR = (1.0f / 4);
+static constexpr float SHIFT_SCALAR = 0.6f;
+
+namespace src::Control {
+
+/**
+ * @brief Gets the current X input from the operator.
+ * As the remote only returns user input every 17ms, analog input is interpolated to get a smoother usable value.
+ *
+ * @return float The current X input from the operator.
+ */
+float OperatorInterface::getChassisXInput() {
+    uint32_t updateCounter = drivers->remote.getUpdateCounter();
+    uint32_t currTime = tap::arch::clock::getTimeMilliseconds();
+
+    lastXInputCallTime = currTime;
+
+    if (prevUpdateCounterX != updateCounter) {
+        chassisXInput.update(drivers->remote.getChannel(Remote::Channel::LEFT_HORIZONTAL), currTime);
+        prevUpdateCounterX = updateCounter;
+    }
+
+    float digitalX = drivers->remote.keyPressed(Remote::Key::D) - drivers->remote.keyPressed(Remote::Key::A);
+
+    float finalX = limitVal<float>(chassisXInput.getInterpolatedValue(currTime) + digitalX, -1.0f, 1.0f);
+
+    // Scales analog values by values defined in hero_constants.hpp to speedshift input
+    finalX *= drivers->remote.keyPressed(Remote::Key::CTRL) ? CTRL_SCALAR : 1.0f;
+    finalX *= drivers->remote.keyPressed(Remote::Key::SHIFT) ? SHIFT_SCALAR : 1.0f;
+
+    chassisXRamp.setTarget(finalX);
+
+    finalXWatch = digitalX;
+
+    if (chassisXRamp.getTarget() == 0.0f)
+        chassisXRamp.update(INPUT_XY_STOP_INC);
+    else
+        chassisXRamp.update(INPUT_X_INC);
+    return chassisXRamp.getValue();
+}
+
+/**
+ * @brief Gets the current Y input from the operator.
+ * As the remote only returns user input every 17ms, analog input is interpolated to get a smoother usable value.
+ *
+ * @return float The current Y input from the operator.
+ */
+float OperatorInterface::getChassisYInput() {
+    uint32_t updateCounter = drivers->remote.getUpdateCounter();
+    uint32_t currTime = tap::arch::clock::getTimeMilliseconds();
+    lastYInputCallTime = currTime;
+
+    if (prevUpdateCounterY != updateCounter) {
+        chassisYInput.update(drivers->remote.getChannel(Remote::Channel::LEFT_VERTICAL), currTime);
+        prevUpdateCounterY = updateCounter;
+    }
+
+    float digitalY = drivers->remote.keyPressed(Remote::Key::W) - drivers->remote.keyPressed(Remote::Key::S);
+
+    float finalY = limitVal<float>(chassisYInput.getInterpolatedValue(currTime) + digitalY, -1.0f, 1.0f);
+
+    // Scales analog values by values defined in hero_constants.hpp to speedshift input
+    finalY *= drivers->remote.keyPressed(Remote::Key::CTRL) ? CTRL_SCALAR : 1.0f;
+    finalY *= drivers->remote.keyPressed(Remote::Key::SHIFT) ? SHIFT_SCALAR : 1.0f;
+
+    chassisYRamp.setTarget(finalY);
+
+    if (chassisYRamp.getTarget() == 0.0f)
+        chassisYRamp.update(INPUT_XY_STOP_INC);
+    else
+        chassisYRamp.update(INPUT_Y_INC);
+    return chassisYRamp.getValue();
+}
+
+/**
+ * @brief Gets the current rotation input from the operator.
+ * As the remote only returns user input every 17ms, analog input is interpolated to get a smoother usable value.
+ *
+ * @return float The current rotation input from the operator.
+ */
+float OperatorInterface::getChassisRotationInput() {
+    uint32_t updateCounter = drivers->remote.getUpdateCounter();
+    uint32_t currTime = tap::arch::clock::getTimeMilliseconds();
+    lastRInputCallTime = currTime;
+
+    if (prevUpdateCounterRotation != updateCounter) {
+        chassisRotationInput.update(drivers->remote.getChannel(Remote::Channel::RIGHT_HORIZONTAL), currTime);
+        prevUpdateCounterRotation = updateCounter;
+    }
+
+    float digitalRotation = drivers->remote.keyPressed(Remote::Key::Z) - drivers->remote.keyPressed(Remote::Key::X);
+
+    float finalRotation =
+        limitVal<float>(chassisRotationInput.getInterpolatedValue(currTime) + digitalRotation, -1.0f, 1.0f);
+    finalRotation *= drivers->remote.keyPressed(Remote::Key::CTRL) ? CTRL_SCALAR : 1.0f;
+
+    chassisRotationRamp.setTarget(finalRotation);
+
+    chassisRotationRamp.update(INPUT_R_INC);
+    return chassisRotationRamp.getValue();
+}
+
+float OperatorInterface::getWristYawInput() {
+    uint32_t updateCounter = drivers->remote.getUpdateCounter();
+    uint32_t currTime = tap::arch::clock::getTimeMilliseconds();
+    lastWristYawInputCallTime = currTime;
+
+    if (prevUpdateCounterWristYaw != updateCounter) {
+        wristYawInput.update(drivers->remote.getChannel(Remote::Channel::LEFT_VERTICAL), currTime);
+        prevUpdateCounterWristYaw = updateCounter;
+    }
+
+    float digitalRotation = drivers->remote.keyPressed(Remote::Key::Z) - drivers->remote.keyPressed(Remote::Key::X);
+
+    float finalRotation = limitVal<float>(wristYawInput.getInterpolatedValue(currTime) + digitalRotation, -1.0f, 1.0f);
+    finalRotation *= drivers->remote.keyPressed(Remote::Key::CTRL) ? CTRL_SCALAR : 1.0f;
+
+    wristYawRotationRamp.setTarget(finalRotation);
+
+    wristYawRotationRamp.update(INPUT_WRIST_YAW_INC);
+    return wristYawRotationRamp.getValue();
+}
+
+float OperatorInterface::getWristPitchInput() {
+    uint32_t updateCounter = drivers->remote.getUpdateCounter();
+    uint32_t currTime = tap::arch::clock::getTimeMilliseconds();
+    lastWristPitchInputCallTime = currTime;
+
+    if (prevUpdateCounterWristPitch != updateCounter) {
+        wristPitchInput.update(drivers->remote.getChannel(Remote::Channel::LEFT_HORIZONTAL), currTime);
+        prevUpdateCounterWristPitch = updateCounter;
+    }
+
+    float digitalRotation = drivers->remote.keyPressed(Remote::Key::Z) - drivers->remote.keyPressed(Remote::Key::X);
+
+    float finalRotation = limitVal<float>(wristPitchInput.getInterpolatedValue(currTime) + digitalRotation, -1.0f, 1.0f);
+    finalRotation *= drivers->remote.keyPressed(Remote::Key::CTRL) ? CTRL_SCALAR : 1.0f;
+
+    wristPitchRotationRamp.setTarget(finalRotation);
+
+    wristPitchRotationRamp.update(INPUT_WRIST_PITCH_INC);
+    return wristPitchRotationRamp.getValue();
+}
+
+float OperatorInterface::getWristRollInput() {
+    uint32_t updateCounter = drivers->remote.getUpdateCounter();
+    uint32_t currTime = tap::arch::clock::getTimeMilliseconds();
+    lastWristRollInputCallTime = currTime;
+
+    if (prevUpdateCounterWristRoll != updateCounter) {
+        wristRollInput.update(drivers->remote.getChannel(Remote::Channel::RIGHT_VERTICAL), currTime);
+        prevUpdateCounterWristRoll = updateCounter;
+    }
+
+    float digitalRotation = drivers->remote.keyPressed(Remote::Key::Z) - drivers->remote.keyPressed(Remote::Key::X);
+
+    float finalRotation = limitVal<float>(wristRollInput.getInterpolatedValue(currTime) + digitalRotation, -1.0f, 1.0f);
+    finalRotation *= drivers->remote.keyPressed(Remote::Key::CTRL) ? CTRL_SCALAR : 1.0f;
+
+    wristRollRotationRamp.setTarget(finalRotation);
+
+    wristRollRotationRamp.update(INPUT_WRIST_ROLL_INC);
+    return wristRollRotationRamp.getValue();
+}
+
+float OperatorInterface::getSlideUpDownInput() {
+    uint32_t updateCounter = drivers->remote.getUpdateCounter();
+    uint32_t currTime = tap::arch::clock::getTimeMilliseconds();
+    lastSlideUpDownInputCallTime = currTime;
+
+    if (prevUpdateCounterSlideUpDown != updateCounter) {
+        slideUpDownInput.update(drivers->remote.getChannel(Remote::Channel::LEFT_VERTICAL), currTime);
+        prevUpdateCounterSlideUpDown = updateCounter;
+    }
+
+    float digitalRotation = drivers->remote.keyPressed(Remote::Key::R) - drivers->remote.keyPressed(Remote::Key::F);
+
+    float finalRotation = limitVal<float>(slideUpDownInput.getInterpolatedValue(currTime) + digitalRotation, -1.0f, 1.0f);
+    finalRotation *= drivers->remote.keyPressed(Remote::Key::CTRL) ? CTRL_SCALAR : 1.0f;
+
+    slideUpDownRamp.setTarget(finalRotation);
+
+    if (slideUpDownRamp.getTarget() == 0.0f)
+        slideUpDownRamp.update(INPUT_SLIDE_STOP_INC);
+    else
+        slideUpDownRamp.update(INPUT_SLIDE_UPDOWN_INC);
+
+    return /*slideUpDownRamp.getValue()*/ finalRotation * ZAXIS_JOYSTICK_INPUT_SENSITIVITY;
+}
+
+float OperatorInterface::getSlideFrontBackInput() {
+    uint32_t updateCounter = drivers->remote.getUpdateCounter();
+    uint32_t currTime = tap::arch::clock::getTimeMilliseconds();
+    lastSlideFrontBackInputCallTime = currTime;
+
+    if (prevUpdateCounterSlideFrontBack != updateCounter) {
+        slideFrontBackInput.update(drivers->remote.getChannel(Remote::Channel::RIGHT_VERTICAL), currTime);
+        prevUpdateCounterSlideFrontBack = updateCounter;
+    }
+
+    float digitalRotation = drivers->remote.keyPressed(Remote::Key::C) - drivers->remote.keyPressed(Remote::Key::V);
+
+    float finalRotation = limitVal<float>(slideFrontBackInput.getInterpolatedValue(currTime) + digitalRotation, -1.0f, 1.0f);
+    finalRotation *= drivers->remote.keyPressed(Remote::Key::CTRL) ? CTRL_SCALAR : 1.0f;
+
+    slideFrontBackRamp.setTarget(finalRotation);
+
+    if (slideUpDownRamp.getTarget() == 0.0f)
+        slideFrontBackRamp.update(INPUT_SLIDE_STOP_INC);
+    else
+        slideFrontBackRamp.update(INPUT_SLIDE_FRONTBACK_INC);
+
+    return /*slideFrontBackRamp.getValue() */ finalRotation * XAXIS_JOYSTICK_INPUT_SENSITIVITY;
+}
+
+int16_t mouseXDisplay = 0;
+int16_t mouseYDisplay = 0;
+float OperatorInterface::getGimbalYawInput() {
+    mouseXFilter.update(drivers->remote.getMouseX());
+    // mouseXDisplay = drivers->remote.getMouseX();
+    mouseXDisplay = mouseXFilter.getValue();
+
+    return drivers->remote.getChannel(Remote::Channel::RIGHT_HORIZONTAL) * YAW_JOYSTICK_INPUT_SENSITIVITY +
+           static_cast<float>(limitVal<int16_t>(mouseXFilter.getValue(), -MOUSE_YAW_MAX, MOUSE_YAW_MAX)) *
+               YAW_MOUSE_INPUT_SENSITIVITY;
+}
+
+float OperatorInterface::getGimbalPitchInput() {
+    mouseYFilter.update(-drivers->remote.getMouseY());
+    mouseYDisplay = mouseYFilter.getValue();
+
+    // mouseYDisplay = drivers->remote.getMouseY();
+    return drivers->remote.getChannel(Remote::Channel::RIGHT_VERTICAL) * PITCH_JOYSTICK_INPUT_SENSITIVITY +
+           static_cast<float>(limitVal<int16_t>(mouseYFilter.getValue(), -MOUSE_PITCH_MAX, MOUSE_PITCH_MAX)) *
+               PITCH_MOUSE_INPUT_SENSITIVITY;
+}
+
+}  // namespace src::Control
+
+#endif
---
+++ aimbots-src/src/informants/controller/robot_control_interface.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+#include "tap/algorithms/linear_interpolation_predictor.hpp"
+#include "tap/algorithms/ramp.hpp"
+#include "tap/architecture/clock.hpp"
+#include "tap/communication/serial/remote.hpp"
+#include "tap/drivers.hpp"
+#include "tap/util_macros.hpp"
+
+#include "utils/filters/ema.hpp"
+
+using namespace tap::algorithms;
+
+namespace src::Control {
+
+class OperatorInterface {
+private:
+    tap::Drivers *drivers;
+
+    uint32_t prevUpdateCounterX = 0;
+    uint32_t prevUpdateCounterY = 0;
+    uint32_t prevUpdateCounterRotation = 0;
+
+    uint32_t prevUpdateCounterWristYaw = 0;
+    uint32_t prevUpdateCounterWristPitch = 0;
+    uint32_t prevUpdateCounterWristRoll = 0;
+    uint32_t prevUpdateCounterSlideUpDown = 0;
+    uint32_t prevUpdateCounterSlideFrontBack = 0;
+
+    uint32_t lastXInputCallTime = 0;
+    uint32_t lastYInputCallTime = 0;
+    uint32_t lastRInputCallTime = 0;
+
+    uint32_t lastWristYawInputCallTime = 0;
+    uint32_t lastWristPitchInputCallTime = 0;
+    uint32_t lastWristRollInputCallTime = 0;
+    uint32_t lastSlideUpDownInputCallTime = 0;
+    uint32_t lastSlideFrontBackInputCallTime = 0;
+
+    LinearInterpolationPredictor chassisXInput;
+    LinearInterpolationPredictor chassisYInput;
+    LinearInterpolationPredictor chassisRotationInput;
+
+    LinearInterpolationPredictor wristYawInput;
+    LinearInterpolationPredictor wristPitchInput;
+    LinearInterpolationPredictor wristRollInput;
+    LinearInterpolationPredictor slideUpDownInput;
+    LinearInterpolationPredictor slideFrontBackInput;
+
+    src::Utils::Filters::EMAFilter mouseXFilter;
+    src::Utils::Filters::EMAFilter mouseYFilter;
+
+    tap::algorithms::Ramp chassisXRamp;
+    tap::algorithms::Ramp chassisYRamp;
+    tap::algorithms::Ramp chassisRotationRamp;
+
+    tap::algorithms::Ramp wristYawRotationRamp;
+    tap::algorithms::Ramp wristPitchRotationRamp;
+    tap::algorithms::Ramp wristRollRotationRamp;
+
+    tap::algorithms::Ramp slideUpDownRamp;
+    tap::algorithms::Ramp slideFrontBackRamp;
+
+public:
+    OperatorInterface(tap::Drivers *drivers)
+        :  //
+          drivers(drivers),
+          mouseXFilter(0.5f),
+          mouseYFilter(0.5f) {}
+    DISALLOW_COPY_AND_ASSIGN(OperatorInterface)
+    mockable ~OperatorInterface() = default;
+
+    mockable float getChassisXInput();
+    mockable float getChassisYInput();
+    mockable float getChassisRotationInput();
+
+    mockable float getWristYawInput();
+    mockable float getWristPitchInput();
+    mockable float getWristRollInput();
+
+    mockable float getSlideUpDownInput();
+    mockable float getSlideFrontBackInput();
+
+    mockable float getGimbalYawInput();
+    mockable float getGimbalPitchInput();
+
+    // Think of the maxes like setting a sensitivity for the mouse
+    // The max defines an operational range of the mouse velocity
+    // The scalar is used in the calculation to determine what percentage of that range is currently being used
+    //(Or so I believe, this is techincally straight copied from UW's code and has yet to be field tested)
+    static constexpr int16_t MOUSE_YAW_MAX = 1000;
+    static constexpr int16_t MOUSE_PITCH_MAX = 1000;
+    static constexpr float YAW_MOUSE_INPUT_SENSITIVITY = (0.15f / MOUSE_YAW_MAX);
+    static constexpr float PITCH_MOUSE_INPUT_SENSITIVITY = (0.1f / MOUSE_PITCH_MAX);
+};
+
+}  // namespace src::Control
