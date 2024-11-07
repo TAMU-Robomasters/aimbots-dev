285e5d601142c68aa02d1837c2ab7790e6700079
svukryggi <dmdiniaco@gmail.com>
2024-11-06 21:06:36 -0600

    Update power_limiter.cpp

--- aimbots-src/src/utils/motion/power_limiter/power_limiter.cpp
+++ aimbots-src/src/utils/motion/power_limiter/power_limiter.cpp
@@ -19,12 +19,17 @@ PowerLimiter::PowerLimiter(
 {}
 
 float PowerLimiter::getPowerLimitRatio() {
+    // Catch for finding new ref info
     if (!drivers->refSerial.getRefSerialReceivingData()) {
         return 1.0f;
     }
 
+    // Updates power expended since previous local refresh & energy buffer
     updatePowerAndEnergyBuffer();
 
+    // Checks if we are cutting close to breaking the 60/250J limit
+    //    If we're close, it cuts down on power
+    //    If we're not, robot keeps truckin' along
     if (energyBuffer < energyBufferLimitThreshold) {
         return limitVal(
             static_cast<float>(energyBuffer - energyBufferCritThreshold) / energyBufferLimitThreshold,
@@ -36,21 +41,25 @@ float PowerLimiter::getPowerLimitRatio() {
 }
 
 void PowerLimiter::updatePowerAndEnergyBuffer() {
-    const auto &robotData = drivers->refSerial.getRobotData();
-    const auto &chassisData = robotData.chassis;
-    const float current = chassisData.current;
-    const float newChassisPower = (chassisData.volt * current) / 1'000'000.0f;
-    // we're multiplying by 1'000'000.0f to convert from microwatts to watts
 
+    // Calculates power consumed between local refresh
+    const auto &robotData = drivers->refSerial.getRobotData();                 // Pulls current data
+    const auto &chassisData = robotData.chassis;                               //
+    const float current = chassisData.current;                                 //
+    const float newChassisPower = (chassisData.volt * current) / 1'000'000.0f; // *1'000'000.0f = convert microwatts to watts
+    
+    // Calculates energy buffer in between ref info updates
     const float dt = tap::arch::clock::getTimeMilliseconds() - prevTime;
     prevTime = tap::arch::clock::getTimeMilliseconds();
     energyBuffer -= ((consumedPower * safetyFactor) - chassisData.powerConsumptionLimit) * dt / 1000.0f;
 
+    // Checks for ref info update. If ref info update is available, it resets our energy buffer
     if (robotData.robotDataReceivedTimestamp != prevRobotDataReceivedTimestamp) {
         energyBuffer = chassisData.powerBuffer;
         prevRobotDataReceivedTimestamp = robotData.robotDataReceivedTimestamp;
     }
 
+    // Updates consumed power
     consumedPower = newChassisPower;
 }
 
