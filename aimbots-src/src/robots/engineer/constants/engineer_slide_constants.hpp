4b9092f54359c44b335bec204571d4dc6c7ba4af
HeroOfWind243 <67348400+HeroOfWind243@users.noreply.github.com>
2025-04-23 12:43:17 -0500

    Engineer high Z slide limit

--- aimbots-src/src/robots/engineer/constants/engineer_slide_constants.hpp
+++ aimbots-src/src/robots/engineer/constants/engineer_slide_constants.hpp
@@ -4,7 +4,6 @@
 
 #define SLIDE_COMPATIBLE
 
-
 static constexpr SmoothPIDConfig SLIDE_X_POSITION_PID_CONFIG = {
     .kp = 30000.0f,
     .ki = 0.0f,
@@ -33,7 +32,6 @@ static constexpr SmoothPIDConfig SLIDE_Z_POSITION_PID_CONFIG = {
     .errorDerivativeFloor = 0.0f,
 };
 
-
 // TODO: set these to what they actually are
 static constexpr uint8_t SLIDE_MOTOR_COUNT = 2;
 static constexpr CANBus SLIDE_BUS = CANBus::CAN_BUS1;
@@ -42,4 +40,4 @@ static constexpr MotorID SLIDE_Z_MOTOR_ID = MotorID::MOTOR5;
 static constexpr bool SLIDE_X_MOTOR_DIRECTION = false;
 static constexpr bool SLIDE_Z_MOTOR_DIRECTION = true;
 static constexpr float SLIDE_METERS_PER_REVS_RATIOS[]{(0.254f / 46.0f), (0.184f / 130.0f)};
-static constexpr float SLIDE_MAX_POSITIONS_METERS[] = {0.254f, 0.18f};
+static constexpr float SLIDE_MAX_POSITIONS_METERS[] = {0.254f, 0.25f};  // 0.254 0.18
