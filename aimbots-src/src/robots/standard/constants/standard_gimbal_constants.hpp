6b54934808f9a2d9fa0f36ff69d9842f41a0daa9
zDagger <jdagcarroll@gmail.com>
2025-06-25 23:20:23 -0700

    fixed blastoise offset

--- aimbots-src/src/robots/standard/constants/standard_gimbal_constants.hpp
+++ aimbots-src/src/robots/standard/constants/standard_gimbal_constants.hpp
@@ -17,7 +17,7 @@ static constexpr uint8_t PITCH_MOTOR_COUNT = 1;
 
 #if defined(TARGET_STANDARD_BLASTOISE)
 /* What motor angles ensures that the barrel is pointing straight forward and level relative to the robot chassis? */
-static const std::array<float, YAW_MOTOR_COUNT> YAW_MOTOR_OFFSET_ANGLES = {modm::toRadian(309.199f)};
+static const std::array<float, YAW_MOTOR_COUNT> YAW_MOTOR_OFFSET_ANGLES = {modm::toRadian(144.27f)};
 static const std::array<float, PITCH_MOTOR_COUNT> PITCH_MOTOR_OFFSET_ANGLES = {modm::toRadian(11.11f)};
 static constexpr float PITCH_AXIS_SOFTSTOP_LOW = modm::toRadian(-14.5f);
 static constexpr float PITCH_AXIS_SOFTSTOP_HIGH = modm::toRadian(17.5f);
