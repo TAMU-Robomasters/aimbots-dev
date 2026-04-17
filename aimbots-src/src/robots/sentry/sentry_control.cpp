48589b68bd2bcd8c6b1fdea6f840a2c1623b4d57
kismitz <kismitz@proton.me>
2026-04-17 18:46:36 -0500

    sentry is so back! :3

--- aimbots-src/src/robots/sentry/sentry_control.cpp
+++ aimbots-src/src/robots/sentry/sentry_control.cpp
@@ -303,9 +303,10 @@ HoldCommandMapping leftSwitchMid(
 
 HoldCommandMapping leftSwitchUp(
     drivers(),
-    {&chassisToggleDriveIgnoreGimbalCommand,&gimbalChaseCommand},
+    //{&chassisToggleDriveIgnoreGimbalCommand,&gimbalChaseCommand},
     //{/*&imuCalibrateCommand,*/ &chassisTokyoCommand, &gimbalFieldRelativeControlCommand},
     // {&gimbalVelocityTunningCommand},
+    {&chassisTokyoCommand, &gimbalChaseCommand2},
     //{/*&chassisTokyoCommand,*/ &matchChassisControlCommand, &matchGimbalControlCommand, &matchFiringControlCommand
     // {&chassisAutoNavCommand, &gimbalToggleAimCommand /*&gimbalChaseCommand*/},
     RemoteMapState(Remote::Switch::LEFT_SWITCH, Remote::SwitchState::UP));
